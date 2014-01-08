50ee4ea0e598f49a9a63dcadc0518aecceaab27a
avinash <avinash.it09@gmail.com>
2014-01-08 19:41:55 +0530

    added models and mock data

--- src/gladminds/models/common.py
+++ src/gladminds/models/common.py
@@ -1,25 +1,34 @@
 from django.db import models
 from django.db.models.fields.related import  ForeignKey,OneToOneField
   
-        
-
-   
 class Customer(models.Model):
-    cid=models.CharField(max_length=215,primary_key=True)
-    phone_number = models.CharField(max_length=10,blank=False, null= False, unique= True)
+    customer_id=models.CharField(max_length=215,primary_key=True)
+    phone_number = models.IntegerField(max_length=10,blank=False, null= False, unique= True)
+    is_authenticated=models.BooleanField(default=False)
     class Meta:
         app_label="gladminds"
-        
-        
+              
 class Product(models.Model):
-    customer=ForeignKey(to=Customer,related_name='customer')
     product_id=models.CharField(max_length=215,primary_key=True) 
-    product_name=models.CharField(max_length=215)
+    brand_name=models.CharField(max_length=215)
     class Meta:
         app_label="gladminds" 
         
 class Service(models.Model):
     product=ForeignKey(to=Product,related_name='product')
-    services_code=models.CharField(max_length=215,primary_key=True)
+    unique_service_code=models.CharField(max_length=215,primary_key=True)
     class Meta:
         app_label="gladminds"  
+        
+class ProductPurchased(models.Model):
+    customer_id=models.CharField(max_length=215,null= False)
+    product_id=models.CharField(max_length=215,null= False)
+    class Meta:
+        app_label="gladminds" 
+        
+class RegisteredDealers(models.Model):
+    phone_number = models.IntegerField(max_length=10,blank=False, null= False, unique= True)
+    class Meta:
+        app_label="gladminds" 
+    
+    
