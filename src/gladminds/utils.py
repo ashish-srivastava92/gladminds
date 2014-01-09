74a85b72bd0d7e4ffa81d1d1bebb9ed674fad5a1
bittu08 <bittukumardh@gmail.com>
2014-01-09 23:42:55 +0530

    Separated the audit log and also log the celery task

--- src/gladminds/utils.py
+++ src/gladminds/utils.py
@@ -1,9 +1,16 @@
 import os
 import hashlib
 from tastypie.serializers import Serializer
+from models import logs
+from datetime import datetime
          
 def generate_unique_customer_id():
     bytes_str = os.urandom(24)
     unique_str = hashlib.md5(bytes_str).hexdigest()[:7]
     return unique_str.upper()
+
+    
+def save_log(**kwargs):
+    action_log = logs.AuditLog(date=datetime.now(), action=kwargs['action'], sender=kwargs['sender'], reciever=kwargs['reciever'], status=kwargs['status'], message=kwargs['message'])
+    action_log.save()
 
