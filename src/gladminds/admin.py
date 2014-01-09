20f08caa254509a710e4e535871021ed7cbdcceb
avinash <avinash.it09@gmail.com>
2014-01-09 17:07:49 +0530

    added models for product purchased and registered dealers

--- src/gladminds/admin.py
+++ src/gladminds/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models.common import Customer,Product,Service
+from models.common import Customer,Product,Service,RegisteredDealers,ProductPurchased
 from suit.widgets import NumberInput
 from suit.admin import SortableModelAdmin
 from django.forms import ModelForm, TextInput
@@ -26,7 +26,18 @@ class ServiceAdmin(ModelAdmin):
     search_fields = ('unique_service_code',)
     list_filter = ('product',)
     list_display = ('product', 'unique_service_code')
+    
+class DealerAdmin(ModelAdmin):
+    search_fields = ('phone_number',)
+    list_display = ('phone_number',)
+    
+class ProductPurchasedAdmin(ModelAdmin):
+    search_fields = ('product_id','customer_id')
+    list_display = ('product_id','customer_id')
+    
 
 admin.site.register(Customer, CustomerAdmin)
 admin.site.register(Product,ProductAdmin)
 admin.site.register(Service,ServiceAdmin)
+admin.site.register(RegisteredDealers,DealerAdmin)
+admin.site.register(ProductPurchased,ProductPurchasedAdmin)
