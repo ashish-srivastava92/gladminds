e37f071fb3b1ff5cfddd329a4502077b14e053c7
bittu08 <bittukumardh@gmail.com>
2014-01-13 12:32:36 +0530

    Added the mysql dependency in setup.py and created prod_settings, which contain mysql configuration for prod environment.

---
+++ src/gladminds/prod_settings.py
@@ -0,0 +1,12 @@
+from dev_settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gladminds',
+        'USER': 'gladminds',
+        'PASSWORD': 'gladminds',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
