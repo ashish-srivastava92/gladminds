f383a50cb81833848ea42242403840c0b2c34c86
bittu08 <bittukumardh@gmail.com>
2014-01-14 19:13:29 +0530

    Added runscript

---
+++ runscript.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Pull latest code changes from Github
+#git pull origin master
+
+# Bootstrap setup
+python bootstrap.py
+
+# Run buildout
+bin/buildout -o
+
+# Synchromize database
+#bin/fab syncdb
+
+# Run collectstatic
+#bin/fab collectstatic
+
+# TODO: Stop already running server
+output=`ps aux | grep "bin/django r[u]nserver 0.0.0.0:8000"`
+set -- $output
+pid=$2
+echo "Stopping gladminds (PID $pid) ..."
+kill $pid
+sleep 2
+kill -9 $pid >/dev/null 2>&1
+sleep 5
+echo "Stopped gladminds"
+
+# Run server
+echo "Starting gladminds ..."
+nohup bin/fab runserver &
+sleep 5
+output=`ps aux | grep "bin/django r[u]nserver 0.0.0.0:8000"`
+set -- $output
+pid=$2
+echo "Started gladminds (PID $pid)"
