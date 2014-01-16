3a3df4b9c78a97e9af9c915a52ac6d6489d24c34
bittu08 <bittukumardh@gmail.com>
2014-01-16 17:03:45 +0530

    Added the middleware which whether the message is coming from SA, Customer or admin and set into request object. Also added 'South' package in setup.py to migrate the schema changes.

--- src/gladminds/middleware.py
+++ src/gladminds/middleware.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from gladminds.models import common
+from gladminds import utils
 import logging
 logger = logging.getLogger(__name__)
 
@@ -13,4 +16,31 @@ class GladmindsMiddleware(object):
         pass
     
     def process_view(self, request, view_func, view_args, view_kwargs):
-        pass
+        if request.user.is_authenticated():
+            print "This is an admin user, which sent this request"
+        
+        if request.path is '/v1/messages':
+            auth_rule = None
+            message = request.body
+            phone_number = None
+            keyword= utils.parse_message(message)
+            handler = settings.HANDLER_MAPPER.get(keyword, None)
+            if handler:
+                auth_rule = handler['auth_rule']
+            
+            if 'open' in auth_rule:
+                pass
+            elif 'sa' in auth_rule:
+                object = common.ServiceAdvisor.objects.get(phone_number=phone_number)
+            elif 'customer' in auth_rule:
+                object = common.GladMindUsers.objects.get(phone_number=phone_number)
+            
+            
+             
+        
+        
+        #If the request is come on message APIs, then it comming from SA or 
+        #Customer. Verify the customer and Dealer as per phone number and message text
+        
+        
+        
