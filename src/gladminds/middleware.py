be87cbf3b34be5c90e0cefec2ade2a2a52641fb0
bittu08 <bittukumardh@gmail.com>
2014-01-16 17:14:22 +0530

    Updated the middleware

--- src/gladminds/middleware.py
+++ src/gladminds/middleware.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from gladminds.models import common
+from django.http import HttpResponseBadRequest
 from gladminds import utils
 import logging
 logger = logging.getLogger(__name__)
@@ -15,10 +16,12 @@ class GladmindsMiddleware(object):
     def __init__(self, *args, **kwargs):
         pass
     
+    #If the request is come on message APIs, then it comming from SA or 
+    #Customer. Verify the customer and Dealer as per phone number and message text
     def process_view(self, request, view_func, view_args, view_kwargs):
         if request.user.is_authenticated():
-            print "This is an admin user, which sent this request"
-        
+            return
+
         if request.path is '/v1/messages':
             auth_rule = None
             message = request.body
@@ -29,18 +32,20 @@ class GladmindsMiddleware(object):
                 auth_rule = handler['auth_rule']
             
             if 'open' in auth_rule:
-                pass
+                return
             elif 'sa' in auth_rule:
-                object = common.ServiceAdvisor.objects.get(phone_number=phone_number)
+                try:
+                    common.ServiceAdvisor.objects.get(phone_number=phone_number)
+                except common.ServiceAdvisor.DoesNotExist:
+                    raise HttpResponseBadRequest() 
+                
             elif 'customer' in auth_rule:
-                object = common.GladMindUsers.objects.get(phone_number=phone_number)
-            
-            
-             
-        
-        
-        #If the request is come on message APIs, then it comming from SA or 
-        #Customer. Verify the customer and Dealer as per phone number and message text
+                try:
+                    object = common.GladMindUsers.objects.get(phone_number=phone_number)
+                except common.GladMindUsers.DoesNotExist:
+                    raise HttpResponseBadRequest()
+                    
+
         
         
         
