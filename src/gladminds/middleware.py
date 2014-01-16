622aeb921b6a3fee9e8655f29a5b94eca75971f1
bittu08 <bittukumardh@gmail.com>
2014-01-16 20:12:06 +0530

    Updated the middleware

--- src/gladminds/middleware.py
+++ src/gladminds/middleware.py
@@ -11,17 +11,14 @@ __all__ = ['GladmindsMiddleware']
 Gladminds middleware to identify the user type (i.e Customer, Service Advisor and Admin). 
 And set the it into request object
 """
-class GladmindsMiddleware(object):
-    
-    def __init__(self, *args, **kwargs):
-        pass
-    
+class GladmindsMiddleware(object):    
     #If the request is come on message APIs, then it comming from SA or 
     #Customer. Verify the customer and Dealer as per phone number and message text
     def process_view(self, request, view_func, view_args, view_kwargs):
+        #If user is authenticated then its a admin user
         if request.user.is_authenticated():
             return
-
+         
         if request.path is '/v1/messages':
             auth_rule = None
             message = request.body
@@ -32,18 +29,25 @@ class GladmindsMiddleware(object):
                 auth_rule = handler['auth_rule']
             
             if 'open' in auth_rule:
+                request.user.username = 'Customer'
                 return
             elif 'sa' in auth_rule:
                 try:
                     common.ServiceAdvisor.objects.get(phone_number=phone_number)
+                    request.user['username'] = 'SA'
+                    request.user['phone_number'] = phone_number
                 except common.ServiceAdvisor.DoesNotExist:
                     raise HttpResponseBadRequest() 
                 
             elif 'customer' in auth_rule:
                 try:
                     object = common.GladMindUsers.objects.get(phone_number=phone_number)
+                    request.user['username'] = 'Customer'
+                    request.user['phone_number'] = phone_number
                 except common.GladMindUsers.DoesNotExist:
                     raise HttpResponseBadRequest()
+        return
+    
                     
 
         
