c9bcc5f6f3edb739b6349a53c7a05e12ce386177
avinash <avinash.it09@gmail.com>
2014-01-20 15:26:11 +0530

    change smsparser logic

--- src/gladminds/smsparser.py
+++ src/gladminds/smsparser.py
@@ -20,7 +20,7 @@ def sms_parser(*args, **kwargs):
         #Added the Message keyword and handler in return dictionary
         key_args.named['keyword'] = lower_keyword
         key_args.named['handler'] = template_mapper[lower_keyword]['handler']
-        key_args.named['auth_rule'] = template_mapper[keyword]['auth_rule']
+        key_args.named['auth_rule'] = template_mapper[lower_keyword]['auth_rule']
 
         return key_args.named
     else:
