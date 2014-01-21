27e7fbef2f944920efe28b67c4477d136d3ad02d
bittu08 <bittukumardh@gmail.com>
2014-01-21 19:08:12 +0530

    Updated the mysql query according to new schema

--- src/gladminds/taskmanager.py
+++ src/gladminds/taskmanager.py
@@ -7,7 +7,7 @@ from gladminds import utils,  message_template as templates
 tuple_to_str = lambda tup: str(tuple(tup)) if len(tup)>1 else "('"+str(tup[0])+"')"
 def get_customers_to_send_reminder(*args, **kwargs):
     from gladminds.tasks import send_reminder_message
-    REMINDER_QUERY = """SELECT gc.id, gu.phone_number, unique_service_coupon, product_id, expired_date, valid_days, valid_kms FROM gladminds_customerdata gc inner join gladminds_gladmindusers gu on gc.phone_number_id = gu.id  WHERE DATE(expired_date) = DATE_ADD(DATE(NOW()),INTERVAL 7 DAY) AND is_closed !=1 AND is_expired!=1 AND last_reminder_date is NULL;"""
+    REMINDER_QUERY = """SELECT gc.id, ggu.phone_number, gc.unique_service_coupon, gu.vin, gc.expired_date, gc.valid_days, gc.valid_kms FROM gladminds_coupondata gc inner join gladminds_customerdata gu on gc.vin_id = gu.id inner join gladminds_gladmindusers ggu on ggu.id = gu.phone_number_id  WHERE DATE(expired_date) = DATE_ADD(DATE(NOW()),INTERVAL 8 DAY) AND status !=2 AND status!=3 AND last_reminder_date is NULL;"""
     usc_list=[]
     cursor = connection.cursor()
     cursor.execute(REMINDER_QUERY)
@@ -15,19 +15,19 @@ def get_customers_to_send_reminder(*args, **kwargs):
     data_list = [dict(zip([col[0] for col in desc], row)) for row in cursor.fetchall()]
     for data in data_list:
         phone_number = data['phone_number']
-        message = templates.REMINDER_COUPON_EXPIRY.format(data['unique_service_coupon'], data['product_id'], data['expired_date'])
+        message = templates.get_template('REMINDER_COUPON_EXPIRY').format(data['unique_service_coupon'], data['product_id'], data['expired_date'])
         send_reminder_message.delay(phone_number = phone_number, message = message)
         usc_list.append(data['unique_service_coupon'])
     
     #After Sending all message into celery queue, update the schedule time in database
     if len(usc_list):
         usc_tup =  tuple_to_str(usc_list)
-        UPDATE_CUSTOMER_DATA = """UPDATE gladminds_customerdata SET last_reminder_date = DATE(NOW()) WHERE unique_service_coupon IN {0};""".format(usc_tup)
+        UPDATE_CUSTOMER_DATA = """UPDATE gladminds_coupondata SET last_reminder_date = DATE(NOW()) WHERE unique_service_coupon IN {0};""".format(usc_tup)
         cursor.execute(UPDATE_CUSTOMER_DATA)
     
 def get_customers_to_send_reminder_by_admin(*args, **kwargs):
     from gladminds.tasks import send_reminder_message
-    SCHEDULE_REMINDER_QUERY = """SELECT gc.id, gu.phone_number, unique_service_coupon, product_id, expired_date, valid_days, valid_kms FROM gladminds_customerdata gc inner join gladminds_gladmindusers gu on gc.phone_number_id = gu.id  WHERE is_closed !=1 AND is_expired!=1 AND schedule_reminder_date = DATE(NOW());"""
+    SCHEDULE_REMINDER_QUERY = """SELECT gc.id, ggu.phone_number, gc.unique_service_coupon, gu.vin, gc.expired_date, gc.valid_days, gc.valid_kms FROM gladminds_coupondata gc inner join gladminds_customerdata gu on gc.vin_id = gu.id inner join gladminds_gladmindusers ggu on ggu.id = gu.phone_number_id  WHERE status !=2 AND status!=3 AND schedule_reminder_date = DATE(NOW());"""
     usc_list=[]
     cursor = connection.cursor()
     cursor.execute(SCHEDULE_REMINDER_QUERY)
@@ -35,18 +35,18 @@ def get_customers_to_send_reminder_by_admin(*args, **kwargs):
     data_list = [dict(zip([col[0] for col in desc], row)) for row in cursor.fetchall()]
     for data in data_list:
         phone_number = data['phone_number']
-        message = templates.REMINDER_COUPON_EXPIRY.format(data['unique_service_coupon'], data['product_id'], data['expired_date'])
+        message = templates.get_template('REMINDER_COUPON_EXPIRY').format(data['unique_service_coupon'], data['product_id'], data['expired_date'])
         send_reminder_message.delay(phone_number = phone_number, message = message)       
         usc_list.append(data['unique_service_coupon'])
     
     #After Sending all message into celery queue, update the schedule time in database
     if len(usc_list):
         usc_tup =  tuple_to_str(usc_list)
-        UPDATE_CUSTOMER_DATA = """UPDATE gladminds_customerdata SET last_reminder_date = DATE(NOW()), schedule_reminder_date=NULL WHERE unique_service_coupon IN {0};""".format(usc_tup)
+        UPDATE_CUSTOMER_DATA = """UPDATE gladminds_coupondata SET last_reminder_date = DATE(NOW()), schedule_reminder_date=NULL WHERE unique_service_coupon IN {0};""".format(usc_tup)
         cursor.execute(UPDATE_CUSTOMER_DATA)
 
 def expire_service_coupon(*args, **kwargs):
-    UPDATE_EXPIRE_STATUS_FOR_SERVICE_COUPON = """UPDATE gladminds_customerdata SET is_expired=1 where expired_date<DATE(NOW())"""
+    UPDATE_EXPIRE_STATUS_FOR_SERVICE_COUPON = """UPDATE gladminds_coupondata SET status=3 where expired_date<DATE(NOW())"""
     try:
         cursor = connection.cursor()
         cursor.execute(UPDATE_EXPIRE_STATUS_FOR_SERVICE_COUPON)
