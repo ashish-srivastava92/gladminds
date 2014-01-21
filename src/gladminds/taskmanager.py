4c2a5bf70df3d1583ea3b0b9afd58fc928e7a80d
bittu08 <bittukumardh@gmail.com>
2014-01-21 21:08:50 +0530

    Set reminder_day

--- src/gladminds/taskmanager.py
+++ src/gladminds/taskmanager.py
@@ -8,10 +8,10 @@ tuple_to_str = lambda tup: str(tuple(tup)) if len(tup)>1 else "('"+str(tup[0])+"
 def get_customers_to_send_reminder(*args, **kwargs):
     from gladminds.tasks import send_reminder_message
     reminder_day = kwargs.get('reminder_day', None) or 7
-    REMINDER_QUERY = """SELECT gc.id, ggu.phone_number, gc.unique_service_coupon, gu.vin, gc.expired_date, gc.valid_days, gc.valid_kms FROM gladminds_coupondata gc inner join gladminds_customerdata gu on gc.vin_id = gu.id inner join gladminds_gladmindusers ggu on ggu.id = gu.phone_number_id  WHERE DATE(expired_date) = DATE_ADD(DATE(NOW()),INTERVAL ? DAY) AND status !=2 AND status!=3 AND last_reminder_date is NULL;"""
+    REMINDER_QUERY = """SELECT gc.id, ggu.phone_number, gc.unique_service_coupon, gu.vin, gc.expired_date, gc.valid_days, gc.valid_kms FROM gladminds_coupondata gc inner join gladminds_customerdata gu on gc.vin_id = gu.id inner join gladminds_gladmindusers ggu on ggu.id = gu.phone_number_id  WHERE DATE(expired_date) = DATE_ADD(DATE(NOW()),INTERVAL {0} DAY) AND status !=2 AND status!=3 AND last_reminder_date is NULL;""".format(reminder_day)
     usc_list=[]
     cursor = connection.cursor()
-    cursor.execute(REMINDER_QUERY, reminder_day)
+    cursor.execute(REMINDER_QUERY)
     desc = cursor.description
     data_list = [dict(zip([col[0] for col in desc], row)) for row in cursor.fetchall()]
     for data in data_list:
