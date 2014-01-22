5b256f7bf1295ecf0769298fd7c56e560032750a
bittu08 <bittukumardh@gmail.com>
2014-01-22 15:54:49 +0530

    Updated the expired_date field in Coupon model to mark_expired_on

--- src/gladminds/taskmanager.py
+++ src/gladminds/taskmanager.py
@@ -8,7 +8,7 @@ tuple_to_str = lambda tup: str(tuple(tup)) if len(tup)>1 else "('"+str(tup[0])+"
 def get_customers_to_send_reminder(*args, **kwargs):
     from gladminds.tasks import send_reminder_message
     reminder_day = kwargs.get('reminder_day', None) or 7
-    REMINDER_QUERY = """SELECT gc.id, ggu.phone_number, gc.unique_service_coupon, gu.vin, gc.expired_date, gc.valid_days, gc.valid_kms FROM gladminds_coupondata gc inner join gladminds_customerdata gu on gc.vin_id = gu.id inner join gladminds_gladmindusers ggu on ggu.id = gu.phone_number_id  WHERE DATE(expired_date) = DATE_ADD(DATE(NOW()),INTERVAL {0} DAY) AND status !=2 AND status!=3 AND last_reminder_date is NULL;""".format(reminder_day)
+    REMINDER_QUERY = """SELECT gc.id, ggu.phone_number, gc.unique_service_coupon, gu.vin, gc.mark_expired_on, gc.valid_days, gc.valid_kms FROM gladminds_coupondata gc inner join gladminds_customerdata gu on gc.vin_id = gu.id inner join gladminds_gladmindusers ggu on ggu.id = gu.phone_number_id  WHERE DATE(mark_expired_on) = DATE_ADD(DATE(NOW()),INTERVAL {0} DAY) AND status !=2 AND status!=3 AND last_reminder_date is NULL;""".format(reminder_day)
     usc_list=[]
     cursor = connection.cursor()
     cursor.execute(REMINDER_QUERY)
@@ -16,7 +16,7 @@ def get_customers_to_send_reminder(*args, **kwargs):
     data_list = [dict(zip([col[0] for col in desc], row)) for row in cursor.fetchall()]
     for data in data_list:
         phone_number = data['phone_number']
-        message = templates.get_template('SEND_CUSTOMER_COUPON_REMINDER').format(data['unique_service_coupon'], data['vin'], data['expired_date'])
+        message = templates.get_template('SEND_CUSTOMER_COUPON_REMINDER').format(data['unique_service_coupon'], data['vin'], data['mark_expired_on'])
         send_reminder_message.delay(phone_number = phone_number, message = message)
         usc_list.append(data['unique_service_coupon'])
     
@@ -28,7 +28,7 @@ def get_customers_to_send_reminder(*args, **kwargs):
     
 def get_customers_to_send_reminder_by_admin(*args, **kwargs):
     from gladminds.tasks import send_reminder_message
-    SCHEDULE_REMINDER_QUERY = """SELECT gc.id, ggu.phone_number, gc.unique_service_coupon, gu.vin, gc.expired_date, gc.valid_days, gc.valid_kms FROM gladminds_coupondata gc inner join gladminds_customerdata gu on gc.vin_id = gu.id inner join gladminds_gladmindusers ggu on ggu.id = gu.phone_number_id  WHERE status !=2 AND status!=3 AND schedule_reminder_date = DATE(NOW());"""
+    SCHEDULE_REMINDER_QUERY = """SELECT gc.id, ggu.phone_number, gc.unique_service_coupon, gu.vin, gc.mark_expired_on, gc.valid_days, gc.valid_kms FROM gladminds_coupondata gc inner join gladminds_customerdata gu on gc.vin_id = gu.id inner join gladminds_gladmindusers ggu on ggu.id = gu.phone_number_id  WHERE status !=2 AND status!=3 AND schedule_reminder_date = DATE(NOW());"""
     usc_list=[]
     cursor = connection.cursor()
     cursor.execute(SCHEDULE_REMINDER_QUERY)
@@ -36,7 +36,7 @@ def get_customers_to_send_reminder_by_admin(*args, **kwargs):
     data_list = [dict(zip([col[0] for col in desc], row)) for row in cursor.fetchall()]
     for data in data_list:
         phone_number = data['phone_number']
-        message = templates.get_template('SEND_CUSTOMER_COUPON_REMINDER').format(data['unique_service_coupon'], data['vin'], data['expired_date'])
+        message = templates.get_template('SEND_CUSTOMER_COUPON_REMINDER').format(data['unique_service_coupon'], data['vin'], data['mark_expired_on'])
         send_reminder_message.delay(phone_number = phone_number, message = message)       
         usc_list.append(data['unique_service_coupon'])
     
@@ -47,7 +47,7 @@ def get_customers_to_send_reminder_by_admin(*args, **kwargs):
         cursor.execute(UPDATE_CUSTOMER_DATA)
 
 def expire_service_coupon(*args, **kwargs):
-    UPDATE_EXPIRE_STATUS_FOR_SERVICE_COUPON = """UPDATE gladminds_coupondata SET status=3 where expired_date<DATE(NOW())"""
+    UPDATE_EXPIRE_STATUS_FOR_SERVICE_COUPON = """UPDATE gladminds_coupondata SET status=3 where mark_expired_on<DATE(NOW())"""
     try:
         cursor = connection.cursor()
         cursor.execute(UPDATE_EXPIRE_STATUS_FOR_SERVICE_COUPON)
