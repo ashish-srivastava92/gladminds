3821f3fc35c3c04249853d92adc5a767dba60d22
bittu08 <bittukumardh@gmail.com>
2014-01-23 19:49:13 +0530

    Updated the CSV importer

--- src/gladminds/import_data.py
+++ src/gladminds/import_data.py
@@ -14,12 +14,12 @@ def import_data(*args, **kwargs):
     
     dealer_sa = csv.DictReader(open(file_path+"/dealer_data.csv"))
     import_dealerdata(dealer_source = dealer_sa)
-    brand_product = csv.DictReader(open(file_path+"/brand_data.csv"))
+    dealer_sa = csv.DictReader(open(file_path+"/dealer_data.csv"))
     import_serviceadvisor_data(sa_source = dealer_sa)
     
     product_coupon = csv.DictReader(open(file_path+"/product_data.csv"))
     import_productdata(product_source = product_coupon)
-    brand_product = csv.DictReader(open(file_path+"/brand_data.csv"))
+    product_coupon = csv.DictReader(open(file_path+"/product_data.csv"))
     import_coupondata(coupon_source = product_coupon)
 
 def import_branddata(brand_source):
@@ -50,18 +50,15 @@ def import_dealerdata(dealer_source):
 def import_serviceadvisor_data(sa_source):
     for sa in sa_source:
         try:
-            print "Saving sa:" % sa
             dealer_data = common.RegisteredDealer.objects.get(dealer_id = sa['dealer_id'])
             service_advisor = common.ServiceAdvisor(dealer_id = dealer_data, service_advisor_id = sa['service_advisor_id'], name = sa['name'], phone_number = sa['phone_number'])
             service_advisor.save()
         except Exception as ex:
-            print "Getting exception in sa type: %s " % ex
             continue
 
 def import_productdata(product_source):
     for product in product_source:
         try:
-            print "Saving product data:" % product
             dealer_data = common.RegisteredDealer.objects.get(dealer_id = product['dealer_id'])
             producttype_data = common.ProductTypeData.objects.get(product_type = product['product_type'])
             customer_phone_number = None if product['customer_phone_number'] is '' else common.GladMindUsers.objects.get(phone_number = product['customer_phone_number'])
@@ -70,13 +67,11 @@ def import_productdata(product_source):
             product_data = common.ProductData(vin = product['vin'], customer_phone_number = customer_phone_number, product_type = producttype_data, sap_customer_id = product['sap_customer_id'], product_purchase_date = product_purchase_date, invoice_date = invoice_date, dealer_id = dealer_data)
             product_data.save()
         except Exception as ex:
-            print "Getting exception in product data: %s " % ex
             continue
 
 def import_coupondata(coupon_source):
     for coupon in coupon_source:
         try:
-            print "Saving coupon data: %s" % coupon
             vin = common.ProductData.objects.get(vin=coupon['vin'])
             status = 1
             closed_date = None if coupon['closed_date'] is '' else datetime.strptime(coupon['closed_date'],'%d-%m-%Y %H:%M:%S')
@@ -85,9 +80,7 @@ def import_coupondata(coupon_source):
             actual_service_date = None if coupon['actual_service_date'] is '' else datetime.strptime(coupon['actual_service_date'],'%d-%m-%Y %H:%M:%S')
             mark_expired_on = None if vin.customer_phone_number is None else datetime.strptime(vin.product_purchase_date) + datetime.timedelta(days=int(coupon['valid_days']))
             sa_phone_number = None if coupon['sa_phone_number'] is '' else common.ServiceAdvisor.objects.get(phone_number = coupon['sa_phone_number'])
-            print "sa_phone_number: %s" % sa_phone_number
             coupon_data = common.CouponData(unique_service_coupon = coupon['unique_service_coupon'], vin = vin, valid_days = coupon['valid_days'], valid_kms = coupon['valid_kms'], service_type = coupon['service_type'], sa_phone_number = sa_phone_number, status = status, closed_date = closed_date, mark_expired_on = mark_expired_on, actual_service_date = actual_service_date, actual_kms = coupon['actual_kms'])
             coupon_data.save()
         except Exception as ex:
-            print "Getting exception in coupon data: %s " % ex
             continue
