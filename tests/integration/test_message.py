6ed18d069c3a025712eb94757ad9ec1be113aea9
avinash <avinash.it09@gmail.com>
2014-01-27 18:21:59 +0530

    adding test file

--- tests/integration/test_message.py
+++ tests/integration/test_message.py
@@ -6,6 +6,8 @@ class GladmindsResourceTestCase(ResourceTestCase):
     
     def setUp(self):
         super(GladmindsResourceTestCase, self).setUp()
+        management.call_command('loaddata', 'etc/testdata/dealer.json', verbosity=0),
+        management.call_command('loaddata', 'etc/testdata/serviceadvisor.json', verbosity=0),
         management.call_command('loaddata', 'etc/data/template.json', verbosity=0)
         self.MESSAGE_URL = "/v1/messages"
     
@@ -33,24 +35,41 @@ class CustomerRegistrationTest(GladmindsResourceTestCase):
         MSG_INVALID_CUST_REG_KEY = "REG test.user@test.com Test User"
         self.INVALID_CUST_REG_KEY = {'text': MSG_INVALID_CUST_REG_KEY, 'phoneNumber': PHONE_NUMBER}
         
-    def test_customer_registration(self):
-       resp = self.api_client.post(uri=self.MESSAGE_URL, data = self.CUST_REG)
-       self.assertHttpOK(resp)
-       
-    def test_invalid_message(self):
-        resp = self.api_client.post(uri=self.MESSAGE_URL, data = self.INVALID_CUST_REG)
-        self.assertHttpBadRequest(resp)
+#     def test_customer_registration(self):
+#        resp = self.api_client.post(uri=self.MESSAGE_URL, data = self.CUST_REG)
+#        self.assertHttpOK(resp)
+#        
+#     def test_invalid_message(self):
+#         resp = self.api_client.post(uri=self.MESSAGE_URL, data = self.INVALID_CUST_REG)
+#         self.assertHttpBadRequest(resp)
+#         
+#         resp = self.api_client.post(uri=self.MESSAGE_URL, data = self.MSG_INVALID_CUST_REG_KEY)
+#         self.assertHttpBadRequest(resp)
         
-        resp = self.api_client.post(uri=self.MESSAGE_URL, data = self.MSG_INVALID_CUST_REG_KEY)
-        self.assertHttpBadRequest(resp)
-
-class CustomerServiceTest(GladmindsResourceTestCase):
+        
+       
+class CouponCheckAndClosure(GladmindsResourceTestCase):
     
     def setUp(self):
-        super(CustomerServiceTest, self).setUp()
-        MSG_CUST_REG = "SERVICE test.user@test.com Test User"
-        PHONE_NUMBER = "+TS{0}".format(int(time.time()))
-        self.CUST_REG = {'text': MSG_CUST_REG, 'phoneNumber': PHONE_NUMBER}
+        super(CouponCheckAndClosure, self).setUp()
+        self.MSG_CHECK_COUPON = "CHECK VIN1 50 1"
+        self.PHONE_NUMBER = "+910000000000"
+        self.CHECK_COUPON = {'text': self.MSG_CHECK_COUPON, 'phoneNumber': self.PHONE_NUMBER}
+    
+        
+    def test_coupon_validation(self):
+       print ":::::::;resp:::::::::::"
+       resp = self.api_client.post(uri=self.MESSAGE_URL, data=self.CHECK_COUPON)
+       print ":::::::;resp:::::::::::",resp
+#        self.assertHttpOK(resp)
+
+# class CustomerServiceTest(GladmindsResourceTestCase):
+#     
+#     def setUp(self):
+#         super(CustomerServiceTest, self).setUp()
+#         MSG_CUST_REG = "SERVICE test.user@test.com Test User"
+#         PHONE_NUMBER = "+TS{0}".format(int(time.time()))
+#         self.CUST_REG = {'text': MSG_CUST_REG, 'phoneNumber': PHONE_NUMBER}
     
     
        
