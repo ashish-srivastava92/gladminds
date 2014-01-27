459c015fe2f44a62a6ca440a58a83a9a9d788fec
bittu08 <bittukumardh@gmail.com>
2014-01-27 16:00:26 +0530

    Added the base class for integration test cases

--- tests/integration/test_message.py
+++ tests/integration/test_message.py
@@ -2,16 +2,33 @@ import time
 from django.core import management
 from tastypie.test import ResourceTestCase
 
-class MessageTest(ResourceTestCase):
+class GladmindsResourceTestCase(ResourceTestCase):
+    
+    def setUp(self):
+        super(GladmindsResourceTestCase, self).setUp()
+        management.call_command('loaddata', 'etc/data/template.json', verbosity=0)
+        self.MESSAGE_URL = "/v1/messages"
+    
+    def assertSuccessfulHttpResponse(self, resp, msg=None):
+        """
+        Ensures the response is returning status between 200 and 299,
+         both inclusive 
+        """
+        return self.assertTrue(resp.status_code >= 200 
+                               and resp.status_code <= 299, msg) 
+        
+
+class CustomerRegistrationTest(GladmindsResourceTestCase):
    
    def setUp(self):
-        super(MessageTest, self).setUp()
-        management.call_command('loaddata', 'etc/data/template.json', verbosity=0)
-        self.URL = "/v1/messages"
+        super(CustomerRegistrationTest, self).setUp()
         self.MSG_CUST_REG = "GCP_REG test.user@test.com Test User"
         self.PHONE_NUMBER = "+TS{0}".format(int(time.time()))
         self.CUST_REG = {'text': self.MSG_CUST_REG, 'phoneNumber': self.PHONE_NUMBER}
+    
         
    def test_customer_registration(self):
-       resp = self.api_client.post(uri=self.URL, data = self.CUST_REG)
+       resp = self.api_client.post(uri=self.MESSAGE_URL, data = self.CUST_REG)
        self.assertHttpOK(resp)
+       
+       
