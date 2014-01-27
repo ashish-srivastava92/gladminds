aafb5cb4fcbc3b719e664beeda999d8b95c00c44
bittu08 <bittukumardh@gmail.com>
2014-01-27 19:02:46 +0530

    Added the test case for customer service

--- tests/integration/test_message.py
+++ tests/integration/test_message.py
@@ -74,9 +74,29 @@ class CustomerServiceTest(GladmindsResourceTestCase):
      
      def setUp(self):
          super(CustomerServiceTest, self).setUp()
-         MSG_CUST_REG = "SERVICE test.user@test.com Test User"
-         PHONE_NUMBER = "+TS{0}".format(int(time.time()))
-         self.CUST_REG = {'text': MSG_CUST_REG, 'phoneNumber': PHONE_NUMBER}
+         VALID_MSG_SERVICE = "SERVICE F0B18AE"
+         VALID_PHONE_NUMBER = "+TS9800000011"
+         self.MSG_SVC = {'text': VALID_MSG_SERVICE, 'phoneNumber': VALID_PHONE_NUMBER}
+         
+         #Invalid check customer id
+         INVALID_MSG_SERVICE = "SERVICE 000000"
+         self.IN_MSG_SVC = {'text': INVALID_MSG_SERVICE, 'phoneNumber': VALID_PHONE_NUMBER}
+         
+         #Invalid Phone Number
+         INVALID_PHONE_NUMBER = "+TA0000000011"
+         self.IN_PH_MSG_SVC = {'text': INVALID_PH_MSG_SERVICE, 'phoneNumber': INVALID_PHONE_NUMBER}
+    
+     def test_valid_service(self):
+        resp = self.api_client.post(uri=self.MESSAGE_URL, data = self.MSG_SVC)
+        self.assertHttpOK(resp)
+    
+     def test_invalid_service(self):
+        resp = self.api_client.post(uri=self.MESSAGE_URL, data = self.IN_MSG_SVC)
+        self.assertHttpBadRequest(resp)
+        
+        resp = self.api_client.post(uri=self.MESSAGE_URL, data = self.IN_PH_MSG_SVC)
+        self.aseertHttpBadRequest(resp)
+        
     
 class CouponCheckAndClosure(GladmindsResourceTestCase):
     def setUp(self):
