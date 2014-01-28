f6716aee94164b8ce2642ca87d70471042e9497d
bittu08 <bittukumardh@gmail.com>
2014-01-28 15:30:56 +0530

    Updated the mark_expired_on date format

--- src/gladminds/taskmanager.py
+++ src/gladminds/taskmanager.py
@@ -8,7 +8,7 @@ tuple_to_str = lambda tup: str(tuple(tup)) if len(tup)>1 else "('"+str(tup[0])+"
 def get_customers_to_send_reminder(*args, **kwargs):
     from gladminds.tasks import send_reminder_message
     reminder_day = kwargs.get('reminder_day', None)
-    REMINDER_QUERY = """SELECT gc.id, ggu.phone_number, gc.unique_service_coupon, gu.vin, gc.mark_expired_on, gc.valid_days, gc.valid_kms FROM gladminds_coupondata gc inner join gladminds_productdata gu on gc.vin_id = gu.id inner join gladminds_gladmindusers ggu on ggu.id = gu.customer_phone_number_id  WHERE DATE(mark_expired_on) = DATE_ADD(DATE(NOW()),INTERVAL {0} DAY) AND status !=2 AND status!=3 AND last_reminder_date is NULL;""".format(reminder_day)
+    REMINDER_QUERY = """SELECT gc.id, ggu.phone_number, gc.unique_service_coupon, gu.vin, DATE_FORMAT(gc.mark_expired_on, '%D %M %Y') as mark_expired_on, gc.valid_days, gc.valid_kms FROM gladminds_coupondata gc inner join gladminds_productdata gu on gc.vin_id = gu.id inner join gladminds_gladmindusers ggu on ggu.id = gu.customer_phone_number_id  WHERE DATE(mark_expired_on) = DATE_ADD(DATE(NOW()),INTERVAL {0} DAY) AND status !=2 AND status!=3 AND last_reminder_date is NULL;""".format(reminder_day)
     usc_list=[]
     cursor = connection.cursor()
     cursor.execute(REMINDER_QUERY)
@@ -28,7 +28,7 @@ def get_customers_to_send_reminder(*args, **kwargs):
     
 def get_customers_to_send_reminder_by_admin(*args, **kwargs):
     from gladminds.tasks import send_reminder_message
-    SCHEDULE_REMINDER_QUERY = """SELECT gc.id, ggu.phone_number, gc.unique_service_coupon, gu.vin, gc.mark_expired_on, gc.valid_days, gc.valid_kms FROM gladminds_coupondata gc inner join gladminds_productdata gu on gc.vin_id = gu.id inner join gladminds_gladmindusers ggu on ggu.id = gu.customer_phone_number_id  WHERE status !=2 AND status!=3 AND DATE(schedule_reminder_date) = DATE(NOW());"""
+    SCHEDULE_REMINDER_QUERY = """SELECT gc.id, ggu.phone_number, gc.unique_service_coupon, gu.vin, DATE_FORMAT(gc.mark_expired_on, '%D %M %Y') as mark_expired_on, gc.valid_days, gc.valid_kms FROM gladminds_coupondata gc inner join gladminds_productdata gu on gc.vin_id = gu.id inner join gladminds_gladmindusers ggu on ggu.id = gu.customer_phone_number_id  WHERE status !=2 AND status!=3 AND DATE(schedule_reminder_date) = DATE(NOW());"""
     usc_list=[]
     cursor = connection.cursor()
     cursor.execute(SCHEDULE_REMINDER_QUERY)
