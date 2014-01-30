b1e2a141123e455a906bf3ba33ad1aac0da4b57b
bittu08 <bittukumardh@gmail.com>
2014-01-30 20:05:20 +0530

    Fetch first avail service coupon

--- src/gladminds/models/common.py
+++ src/gladminds/models/common.py
@@ -9,9 +9,9 @@ class UploadProductCSV(models.Model):
     file_location=settings.PROJECT_DIR+'/data/'
     upload_brand_feed= models.FileField(upload_to=file_location, blank=True)
     upload_dealer_feed=models.FileField(upload_to=file_location, blank=True)
-    upload_product_feed= models.FileField(upload_to=file_location, blank=True)
+    upload_product_dispatch_feed= models.FileField(upload_to=file_location, blank=True)
     upload_product_purchase_feed= models.FileField(upload_to=file_location, blank=True)
-    upload_product_service_feed= models.FileField(upload_to=file_location, blank=True)
+    upload_coupon_redeem_feed= models.FileField(upload_to=file_location, blank=True)
     class Meta:
         app_label = "gladminds"
         verbose_name_plural = "Upload Product Data"
