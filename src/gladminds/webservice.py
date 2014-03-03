8a0e212f4d1452cdc39248af05f8b9172ebf2021
avinash <avinash.it09@gmail.com>
2014-03-04 00:09:16 +0530

    added +91 in dealer feed

--- src/gladminds/webservice.py
+++ src/gladminds/webservice.py
@@ -124,7 +124,7 @@ class DealerService(ServiceBase):
                     'address' : dealer.ADDRESS,
                     'service_advisor_id' : dealer.SER_ADV_ID,
                     'name' : dealer.SER_ADV_NAME,
-                    'phone_number': dealer.SER_ADV_MOBILE
+                    'phone_number': '+91'+dealer.SER_ADV_MOBILE
                 })
             save_to_db(feed_type = 'dealer', data_source = dealer_list)
             return success
