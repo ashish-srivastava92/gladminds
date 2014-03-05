f5be5d91358dc716d15ccfbc87dffa74180b849f
sourabhgupta90 <sourabh5588gupta@gmail.com>
2014-03-05 19:51:24 +0530

    added android build

--- afterbuy_script/afterbuy/platforms/android/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-/home/sattva/workspace/python/gladminds/afterbuy_script/afterbuy/platforms/android/gen/com/domainname/afterbuy/R.java \
- : /home/sattva/workspace/python/gladminds/afterbuy_script/afterbuy/platforms/android/res/drawable-hdpi/icon.png \
-/home/sattva/workspace/python/gladminds/afterbuy_script/afterbuy/platforms/android/res/xml/config.xml \
-/home/sattva/workspace/python/gladminds/afterbuy_script/afterbuy/platforms/android/res/drawable/icon.png \
-/home/sattva/workspace/python/gladminds/afterbuy_script/afterbuy/platforms/android/res/drawable-mdpi/icon.png \
-/home/sattva/workspace/python/gladminds/afterbuy_script/afterbuy/platforms/android/res/values/strings.xml \
-/home/sattva/workspace/python/gladminds/afterbuy_script/afterbuy/platforms/android/res/drawable-xhdpi/icon.png \
-/home/sattva/workspace/python/gladminds/afterbuy_script/afterbuy/platforms/android/res/drawable-ldpi/icon.png \
-/home/sattva/workspace/python/gladminds/afterbuy_script/afterbuy/platforms/android/bin/AndroidManifest.xml \
