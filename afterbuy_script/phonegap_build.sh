90181e4ae9f40b434af3d509063f57029a8739fd
sourabhgupta90 <sourabh5588gupta@gmail.com>
2014-03-07 13:38:06 +0530

    uploaded to s3 successfully

--- afterbuy_script/phonegap_build.sh
+++ afterbuy_script/phonegap_build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Script to create phonegap app
 cd afterbuy_script
+rm -r afterbuy
 phonegap create afterbuy -n afterbuy -i com.gladminds.afterbuy
 cp -r new/* afterbuy/www 
 python parse_afterbuy.py $1  
@@ -10,4 +11,5 @@ cd ../
 zip -r afterbuy afterbuy
 cd ../
 bin/python afterbuy_script/publish_adobe.py $1
+bin/python afterbuy_script/apk_upload.py $1
 exit 0
