3fab66a0a6596e40d5cfeba53a505bcdda3ea6d4
sourabhgupta90 <sourabh5588gupta@gmail.com>
2014-03-11 16:42:19 +0530

    covernt http - https:

--- upload_afterbuy.py
+++ upload_afterbuy.py
@@ -13,11 +13,12 @@ def uploadResultToS3(awsid, awskey, bucket, source_folder):
     c = boto.connect_s3(awsid, awskey)
     b = c.get_bucket(bucket)
     k = Key(b)
+    
     for path, dir, files in os.walk(source_folder):
+        print "sending :", path, 
+        print dir, files
         for upload_file in files:
             relpath = os.path.relpath(os.path.join(path, upload_file))
-            print relpath
-            #print "sending :", path, dir, files
             if not b.get_key(relpath):
                 k.key = relpath
                 k.set_contents_from_filename(relpath)
@@ -26,9 +27,6 @@ def uploadResultToS3(awsid, awskey, bucket, source_folder):
                 except:
                     failed.write(relpath + ', ')
     failed.close()
-
-if os.path.isfile('%s' % machine):
-    os.remove('%s' % machine)
 
 uploadResultToS3('AKIAIL7IDCSTNCG2R6JA', '+5iYfw0LzN8gPNONTSEtyUfmsauUchW1bLX3QL9A',\
                   "afterbuy", '%s' % machine)
