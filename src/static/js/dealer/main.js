18d00e5dcf2ba485d14f3b66c246b8f60d7f6a2d
Shubham <shubham.lakhiwal@hashedin.com>
2014-03-25 12:57:46 +0530

    Implemented functionality on service advisor actions.

--- src/static/js/dealer/main.js
+++ src/static/js/dealer/main.js
@@ -1,4 +1,18 @@
 (function (){
+
+  $("input.advisor-action").click(function() {
+    var actionSet = $(this).parents(".advisor-action-item"),
+        postAction = $(this).val(),
+        sibblingActionSet = actionSet.siblings(".advisor-action-item")
+        disabledInputs = actionSet.find("input[type='text']")
+        activeInputs = sibblingActionSet.find("input[type='text']");
+
+        activeInputs.attr("disabled", "disabled");
+        disabledInputs.removeAttr("disabled");
+  });
+
+  
+
   $(document).ready(function() {
     
   });
