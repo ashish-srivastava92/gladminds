f1e2c08fa962b6f4bfff608dcbbb12a5a4dd65a2
Sourabh Gupta <sourabh5588gupta@gmail.com>
2014-04-11 13:11:04 +0530

    change in afterscript

--- afterbuy_script/phonegap_build.sh
+++ afterbuy_script/phonegap_build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # Script to create phonegap app
 cd afterbuy_script
-rm -r afterbuy
+sudo rm -r afterbuy
+sudo rm -r afterbuy-ui
 phonegap create afterbuy -n afterbuy -i com.gladminds.afterbuy
 git clone git@git.labs.hashedin.com:gladminds/afterbuy.git afterbuy-ui
-cp -r afterbuy-ui/* afterbuy/www 
-python parse_afterbuy.py $1  
+sudo cp -r afterbuy-ui/* afterbuy/www 
+sudo python parse_afterbuy.py $1  
 cd afterbuy 
 cd ../
 zip -r afterbuy afterbuy
