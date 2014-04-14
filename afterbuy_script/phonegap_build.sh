fb0dc1edf66280aaaaef10ffdae11d1f370c754c
Sourabh Gupta <sourabh5588gupta@gmail.com>
2014-04-14 14:18:22 +0530

    change in jenkin app build

--- afterbuy_script/phonegap_build.sh
+++ afterbuy_script/phonegap_build.sh
@@ -3,14 +3,14 @@
 cd afterbuy_script
 sudo rm -r afterbuy
 sudo rm -r afterbuy-ui
-phonegap create afterbuy -n afterbuy -i com.gladminds.afterbuy
 git clone git@git.labs.hashedin.com:gladminds-afterbuy/afterbuyui.git afterbuy-ui
+phonegap create afterbuy -n afterbuy -i com.gladminds.afterbuy
 sudo cp -r afterbuy-ui/* afterbuy/www 
 sudo python parse_afterbuy.py $1  
 cd afterbuy 
 cd ../
 zip -r afterbuy afterbuy
 cd ../
-#bin/python afterbuy_script/publish_adobe.py $1
-#bin/python afterbuy_script/apk_upload.py $1
+bin/python afterbuy_script/publish_adobe.py $1
+bin/python afterbuy_script/apk_upload.py $1
 exit 0
