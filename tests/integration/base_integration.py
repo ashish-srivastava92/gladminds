ae0c1f951a200f5eae74528535c64bba14e653f0
Shubham <shubham.lakhiwal@hashedin.com>
2014-04-16 15:10:57 +0530

    Integration base function modified.

--- tests/integration/base_integration.py
+++ tests/integration/base_integration.py
@@ -45,41 +45,37 @@ class GladmindsResourceTestCase(ResourceTestCase):
         return coupon_obj
 
     def get_product_obj(self, **kwargs):
-        product_data = common.ProductData(
-                    vin=kwargs['vin'], product_type=kwargs['producttype_data'], dealer_id=kwargs['dealer_data'], customer_phone_number=kwargs['customer_phone_number'], sap_customer_id=kwargs['sap_customer_id'])
+        product_data = common.ProductData(**kwargs)
         product_data.save()
         return product_data
 
     def get_delear_obj(self, **kwargs):
-        delear_data = common.RegisteredDealer(dealer_id=kwargs['dealer_id'])
+        delear_data = common.RegisteredDealer(**kwargs)
         delear_data.save()
         return delear_data
 
     def get_product_type_obj(self, **kwargs):
-        product_type_data_obj = common.ProductTypeData(brand_id=kwargs['brand_id'], product_name=kwargs['product_name'], product_type=kwargs['product_type'])
+        product_type_data_obj = common.ProductTypeData(**kwargs)
         product_type_data_obj.save()
         return product_type_data_obj
 
     def get_brand_obj(self, **kwargs):
-        brand_obj = common.BrandData(
-                    brand_id=kwargs['brand_id'], brand_name=kwargs['brand_name'])
+        brand_obj = common.BrandData(**kwargs)
         brand_obj.save()
         return brand_obj
 
     def get_service_advisor_obj(self, **kwargs):
-        service_advisor_obj = common.ServiceAdvisor(service_advisor_id=kwargs[
-                                    'service_advisor_id'], name=kwargs['name'], phone_number=kwargs['phone_number'])
+        service_advisor_obj = common.ServiceAdvisor(**kwargs)
         service_advisor_obj.save()
         return service_advisor_obj
 
     def get_dealer_service_advisor_obj(self, **kwargs):
-        dealer_service_advisor_obj = common.ServiceAdvisorDealerRelationship(dealer_id=kwargs['dealer_data'], service_advisor_id=kwargs[
-                                    'service_advisor_id'], status=kwargs['status'])
+        dealer_service_advisor_obj = common.ServiceAdvisorDealerRelationship(**kwargs)
         dealer_service_advisor_obj.save()
         return dealer_service_advisor_obj
 
     def get_customer_obj(self, **kwargs):
-        customer_obj = common.GladMindUsers(phone_number=kwargs['phone_number'])
+        customer_obj = common.GladMindUsers(**kwargs)
         customer_obj.save()
         return customer_obj
     
--- tests/unit/test_resource.py
+++ tests/unit/test_resource.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from integration.base_integration import GladmindsResourceTestCase
 from datetime import datetime, timedelta
-from django.utils import timezone
 
 client = Client()
 
@@ -20,19 +19,17 @@ class GladmindsResourcesTest(GladmindsResourceTestCase):
         product_type_obj = self.get_product_type_obj(brand_id=brand_obj, product_name='DISCO120', product_type='BIKE')
         dealer_obj = self.get_delear_obj(dealer_id='DEALER001')
         customer_obj = self.get_customer_obj(phone_number='9999999')
-        product_obj = self.get_product_obj(vin="VINXXX001", producttype_data=product_type_obj, dealer_data = dealer_obj\
+        product_obj = self.get_product_obj(vin="VINXXX001", product_type=product_type_obj, dealer_id = dealer_obj\
                                            , customer_phone_number = customer_obj, sap_customer_id='SAP001')
-        service_advisor = self.get_service_advisor_obj(dealer_data = dealer_obj, service_advisor_id = 'SA001Test'\
-                                                 ,name='UMOTO', phone_number='+914444861111', status='Y')
-        self.get_dealer_service_advisor_obj(dealer_data=dealer_obj, service_advisor_id=service_advisor, status='Y')
-        service_advisor1 = self.get_service_advisor_obj(dealer_data = dealer_obj, service_advisor_id = 'SA002Test'\
-                                                 ,name='UMOTOR', phone_number='+919999999999', status='Y')
-        self.get_dealer_service_advisor_obj(dealer_data=dealer_obj, service_advisor_id=service_advisor1, status='Y')
+        service_advisor = self.get_service_advisor_obj(service_advisor_id = 'SA001Test', name='UMOTO', phone_number='+914444861111')
+        self.get_dealer_service_advisor_obj(dealer_id=dealer_obj, service_advisor_id=service_advisor, status='Y')
+        service_advisor1 = self.get_service_advisor_obj(service_advisor_id = 'SA002Test', name='UMOTOR', phone_number='+919999999999')
+        self.get_dealer_service_advisor_obj(dealer_id=dealer_obj, service_advisor_id=service_advisor1, status='Y')
         self.get_coupon_obj(unique_service_coupon='COUPON005', vin=product_obj, valid_days=30, valid_kms=500\
                             , service_type = 1, status=1, mark_expired_on=datetime.now()-timedelta(days=2)\
                             , actual_service_date=datetime.now()-timedelta(days=20))
         customer_obj1 = self.get_customer_obj(phone_number='8888888')
-        product_obj1 = self.get_product_obj(vin="VINXXX002", producttype_data=product_type_obj, dealer_data = dealer_obj\
+        product_obj1 = self.get_product_obj(vin="VINXXX002", product_type=product_type_obj, dealer_id = dealer_obj\
                                            , customer_phone_number = customer_obj1, sap_customer_id='SAP002')
         self.get_coupon_obj(unique_service_coupon='COUPON004', vin=product_obj1, valid_days=30, valid_kms=500\
                             , service_type = 1, status=1, mark_expired_on=datetime.now()-timedelta(days=2)\
