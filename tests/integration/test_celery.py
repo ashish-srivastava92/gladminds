b2087dbbc3e8c7efed956c7e873f1304b9ccb9d2
Sourabh Gupta <sourabh5588gupta@gmail.com>
2014-04-16 17:20:43 +0530

    GM-75: Move Coupon to Exceeds limit status when Actual KMs run exceeds the limit during the CHECK action

--- tests/integration/test_celery.py
+++ tests/integration/test_celery.py
@@ -27,14 +27,14 @@ class CeleryTestCaseBase(GladmindsResourceTestCase):
 
     def test_delay_message(self):
         self.brand_obj = self.get_brand_obj(brand_id='brand001', brand_name='bajaj')
-        self.product_type_obj = self.get_product_type_obj(brand_id=self.brand_obj, product_name='DISCO120', product_type='BIKE')
+        product_type_obj = self.get_product_type_obj(brand_id=self.brand_obj, product_name='DISCO120', product_type='BIKE')
         self.dealer_obj = self.get_delear_obj(dealer_id='DEALER001')
         self.customer_obj = self.get_customer_obj(phone_number='232323232')
-        self.product_obj = self.get_product_obj(vin="VINXXX001", producttype_data=self.product_type_obj, dealer_data = self.dealer_obj, customer_phone_number = self.customer_obj, sap_customer_id='SAP001')
-        self.get_coupon_obj(unique_service_coupon='USC001', product_data=self.product_obj, valid_days=30, valid_kms=500, service_type=1)
+        self.product_obj = self.get_product_obj(vin="VINXXX001", product_type=product_type_obj, dealer_id = self.dealer_obj, customer_phone_number = self.customer_obj, sap_customer_id='SAP001')
+        self.get_coupon_obj(unique_service_coupon='USC001', vin=self.product_obj, valid_days=30, valid_kms=500, service_type=1)
 
         sa_obj = self.get_service_advisor_obj(service_advisor_id='DEALER001SA001', name="SA001", phone_number='9999999')
-        self.get_dealer_service_advisor_obj(dealer_data=self.dealer_obj, service_advisor_id=sa_obj, status='Y')
+        self.get_dealer_service_advisor_obj(dealer_id=self.dealer_obj, service_advisor_id=sa_obj, status='Y')
 
         phone_number = "9999999"
         obj = GladmindsResources()
