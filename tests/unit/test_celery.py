e690a1852835645f57dd3331769f0a7499dcf131
Shubham <shubham.lakhiwal@hashedin.com>
2014-04-24 17:32:04 +0530

    Test cases written

--- tests/unit/test_celery.py
+++ tests/unit/test_celery.py
@@ -1,12 +1,13 @@
 from integration.base_integration import GladmindsResourceTestCase
 from django.test.utils import override_settings
-from gladminds.tasks import send_service_detail, expire_service_coupon
+from gladminds import tasks
 from datetime import datetime, timedelta
 from unit.base_unit import GladmindsUnitTestCase
 from gladminds.utils import COUPON_STATUS
 from gladminds.taskmanager import get_data_feed_log_detail
 from gladminds.models import common
 from django.db.models import Q
+import time
 
 
 class TestCelery(GladmindsResourceTestCase):
@@ -15,13 +16,13 @@ class TestCelery(GladmindsResourceTestCase):
                        CELERY_ALWAYS_EAGER=True,
                        BROKER_BACKEND='django')
     def test_send_sms(self):
-        result = send_service_detail.delay(
+        result = tasks.send_service_detail.delay(
             phone_number='99999999', message='Test Message')
         self.assertTrue(result.successful())
 
     @override_settings(SMS_CLIENT="MOCK_FAIL")
     def test_send_sms_fail(self):
-        result = send_service_detail.delay(
+        result = tasks.send_service_detail.delay(
             phone_number='99999999', message='Test Message')
         self.assertFalse(result.successful())
 
@@ -86,4 +87,25 @@ class TestCronjobs(GladmindsUnitTestCase):
                                     ,success_data_count=4,timestamp=datetime.now(),action='Recieved')
         feeds = get_data_feed_log_detail(start_date=datetime.now()-timedelta(days=1), end_date=datetime.now()+timedelta(days=1))
         self.assertEqual(len(feeds), 1)
+    
+
+class TestTasks(GladmindsUnitTestCase):
+    def setUp(self):
+        super(TestTasks, self).setUp()
+        self.phone_number = '+T0{0}'.format(str(time.time()))
+        self.message = 'Thankyou message'
+    
+    def test_tasks(self):
+        tasks.send_registration_detail(message=self.message,phone_number=self.phone_number)
+        tasks.send_service_detail(message=self.message,phone_number=self.phone_number)
+        tasks.send_coupon_validity_detail(message=self.message,phone_number=self.phone_number)
+        tasks.send_coupon_detail_customer(message=self.message,phone_number=self.phone_number)
+        tasks.send_reminder_message(message=self.message,phone_number=self.phone_number)
+        tasks.send_coupon_close_message(message=self.message,phone_number=self.phone_number)
+        tasks.send_coupon(message=self.message,phone_number=self.phone_number)
+        tasks.send_close_sms_customer(message=self.message,phone_number=self.phone_number)
+        tasks.send_brand_sms_customer(message=self.message,phone_number=self.phone_number)
+        tasks.send_invalid_keyword_message(message=self.message,phone_number=self.phone_number)
+        tasks.send_on_product_purchase(message=self.message,phone_number=self.phone_number)
+        
     
