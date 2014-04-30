90a1d46334ef84566fb77df6d05192aef633c18d
Sourabh Gupta <sourabh5588gupta@gmail.com>
2014-04-30 20:19:34 +0530

    GM-128 Create WSDL for registration of Customer, SA and ASC

--- src/gladminds/webservice.py
+++ src/gladminds/webservice.py
@@ -4,7 +4,7 @@ from spyne.decorator import srpc
 from spyne.model.complex import Array
 from spyne.model.complex import ComplexModel
 from spyne.model.complex import Iterable
-from spyne.model.primitive import Integer,Decimal, Date
+from spyne.model.primitive import Integer, Decimal, Date
 from spyne.model.primitive import Unicode, Mandatory
 from spyne.protocol.soap import Soap11
 from spyne.server.django import DjangoApplication
@@ -23,29 +23,108 @@ from gladminds import utils
 logger = logging.getLogger("gladminds")
 
 
-
 pattern = r'(\d{4})-(\d{2})-(\d{2})(\d{2})(\d{2})(\d{2})'
 tns = "http://api.gladmindsplatform.co/api/v1/bajaj/feed/"
 success = "SUCCESS"
 failed = "FAILURE"
 
+
 class AuthenticationModel(ComplexModel):
     __namespace__ = tns
     UserName = Unicode(min_occurs=1, nillable=False)
     Password = Unicode(min_occurs=1, nillable=False)
-        
+
+######################################MOCK SERVICE################################################
+"""
+    Mock service is created to give WSDL File to BAJAJ. 
+    There is not other use of it.
+"""
+
+
+class ASCRegistrationModel(ComplexModel):
+    __namespace__ = tns
+    ASC_NAME = Unicode
+    ASC_MOBILE = Unicode
+    ASC_EMAIL = Unicode
+    ASC_ADDRESS = Unicode
+    ASC_ADDRESS_PINCODE = Unicode
+    KUNNAR = Unicode
+
+
+class SARegistrationModel(ComplexModel):
+    __namespace__ = tns
+    SER_ADV_NAME = Unicode
+    SER_ADV_MOBILE = Unicode
+    ADDRESS = Unicode
+    ACTIVE_FLAG = Unicode
+    KUNNR = Unicode
+    ASC_ID = Unicode
+
+
+class CustomerRegistrationModel(ComplexModel):
+    __namespace__ = tns
+    CUST_MOBILE = Unicode
+    VEH_SL_DT = Date
+    CHASSIS = Unicode
+    CUSTOMER_NAME = Unicode
+    CITY = Unicode
+    STATE = Unicode
+    PIN_NO = Unicode
+    VEH_REG_NO = Unicode
+    VEH_SL_DLR = Unicode
+    KUNNR = Unicode
+    ENGINE = Unicode
+    ASC_ID = Unicode
+
+
+class ASCRegisterationService(ServiceBase):
+    __namespace__ = tns
+
+    @srpc(ASCRegistrationModel, AuthenticationModel, _returns=Unicode)
+    def ascRegistration(ObjectList, Credential):
+        pass
+
+
+class SARegisterationService(ServiceBase):
+    __namespace__ = tns
+
+    @srpc(SARegistrationModel, AuthenticationModel, _returns=Unicode)
+    def saRegistration(ObjectList, Credential):
+        pass
+
+
+class CustomerRegisterationService(ServiceBase):
+    __namespace__ = tns
+
+    @srpc(CustomerRegistrationModel, AuthenticationModel, _returns=Unicode)
+    def customerRegistration(ObjectList, Credential):
+        pass
+
+mock_app = Application([ASCRegisterationService, SARegisterationService, CustomerRegisterationService],
+                      tns=tns,
+                      in_protocol=Soap11(validator='lxml'),
+                      out_protocol=Soap11()
+                      )
+
+mock_service = csrf_exempt(DjangoApplication(mock_app))
+
+######################################MOCK SERVICE################################################
+
+
 class BrandModel(ComplexModel):
     __namespace__ = tns
     BRAND_ID = Unicode
     BRAND_NAME = Unicode
     PRODUCT_TYPE = Unicode
     PRODUCT_NAME = Unicode
-    TIMESTAMP = Unicode(pattern = pattern)
+    TIMESTAMP = Unicode(pattern=pattern)
+
 
 class BrandModelList(ComplexModel):
     __namespace__ = tns
     BrandData = Array(BrandModel)
-    
+
+
 class DealerModel(ComplexModel):
     __namespace__ = tns
     KUNNR = Unicode
@@ -54,11 +133,13 @@ class DealerModel(ComplexModel):
     SER_ADV_NAME = Unicode
     SER_ADV_MOBILE = Unicode
     ACTIVE_FLAG = Unicode
-    TIMESTAMP = Unicode(pattern = pattern)
+    TIMESTAMP = Unicode(pattern=pattern)
+
 
 class DealerModelList(ComplexModel):
     __namespace__ = tns
     DealerData = Array(DealerModel)
+
 
 class ProductDispatchModel(ComplexModel):
     __namespace__ = tns
@@ -80,6 +161,7 @@ class ProductDispatchModelList(ComplexModel):
     __namespace__ = tns
     ProductDispatchData = Array(ProductDispatchModel)
 
+
 class ProductPurchaseModel(ComplexModel):
     __namespace__ = tns
     CHASSIS = Unicode
@@ -93,8 +175,9 @@ class ProductPurchaseModel(ComplexModel):
     VEH_REG_NO = Unicode
     VEH_SL_DLR = Unicode
     KUNNR = Unicode
-    TIMESTAMP = Unicode(pattern = pattern)
+    TIMESTAMP = Unicode(pattern=pattern)
     ENGINE = Unicode
+
 
 class ProductPurchaseModelList(ComplexModel):
     __namespace__ = tns
@@ -103,18 +186,19 @@ class ProductPurchaseModelList(ComplexModel):
 
 class BrandService(ServiceBase):
     __namespace__ = tns
+
     @srpc(BrandModelList, AuthenticationModel,  _returns=Unicode)
     def postBrand(ObjectList, Credential):
         try:
-            brand_list = [] 
+            brand_list = []
             for brand in ObjectList.BrandData:
                 brand_list.append({
-                    'brand_id':brand.BRAND_ID, 
-                    'brand_name': brand.BRAND_NAME, 
-                    'product_type': brand.PRODUCT_TYPE, 
-                    'product_name'  : brand.PRODUCT_NAME,                
+                    'brand_id': brand.BRAND_ID,
+                    'brand_name': brand.BRAND_NAME,
+                    'product_type': brand.PRODUCT_TYPE,
+                    'product_name': brand.PRODUCT_NAME,
                 })
-            save_to_db(feed_type = 'brand', data_source = brand_list)
+            save_to_db(feed_type='brand', data_source=brand_list)
             return success
         except Exception as ex:
             return failed
@@ -123,50 +207,54 @@ class BrandService(ServiceBase):
 class DealerService(ServiceBase):
     __namespace__ = tns
 
-    @srpc(DealerModelList, AuthenticationModel,_returns=Unicode)
-    def postDealer(ObjectList,Credential):
+    @srpc(DealerModelList, AuthenticationModel, _returns=Unicode)
+    def postDealer(ObjectList, Credential):
         try:
             dealer_list = []
             for dealer in ObjectList.DealerData:
                 dealer_list.append({
-                    'dealer_id' : dealer.KUNNR,
-                    'address' : dealer.ADDRESS,
-                    'service_advisor_id' : dealer.SER_ADV_ID,
-                    'name' : dealer.SER_ADV_NAME,
+                    'dealer_id': dealer.KUNNR,
+                    'address': dealer.ADDRESS,
+                    'service_advisor_id': dealer.SER_ADV_ID,
+                    'name': dealer.SER_ADV_NAME,
                     'phone_number': utils.mobile_format(dealer.SER_ADV_MOBILE),
                     'status': dealer.ACTIVE_FLAG
                 })
-            response = save_to_db(feed_type = 'dealer', data_source = dealer_list)
+            response = save_to_db(feed_type='dealer', data_source=dealer_list)
             return json.dumps(get_response(response))
         except Exception as ex:
-            logging.error("DealerService: {0}  Error on Validating ".format(ex))
-            logger.debug("DealerService: Object List is "\
+            logging.error(
+                "DealerService: {0}  Error on Validating ".format(ex))
+            logger.debug("DealerService: Object List is "
                          .format(ObjectList.DealerData))
             return failed
 
 
 class ProductDispatchService(ServiceBase):
     __namespace__ = tns
-    @srpc(ProductDispatchModelList,AuthenticationModel, _returns=Unicode)
+
+    @srpc(ProductDispatchModelList, AuthenticationModel, _returns=Unicode)
     def postProductDispatch(ObjectList, Credential):
         try:
             product_dispatch_list = []
-            for product in  ObjectList.ProductDispatchData:
+            for product in ObjectList.ProductDispatchData:
                 product_dispatch_list.append({
-                        'vin' : product.CHASSIS,
-                        'product_type': product.PRODUCT_TYPE,
-                        'invoice_date': product.VEC_DIS_DT,
-                        'dealer_id' : product.KUNNR,
-                        'unique_service_coupon' : product.UCN_NO,
-                        'valid_days' : product.DAYS_LIMIT_TO,
-                        'valid_kms' : product.KMS_TO,
-                        'service_type' : product.SERVICE_TYPE,
-                        'coupon_status' : settings.DEFAULT_COUPON_STATUS,
-                    })
-            response = save_to_db(feed_type = 'dispatch', data_source = product_dispatch_list)
+                    'vin': product.CHASSIS,
+                    'product_type': product.PRODUCT_TYPE,
+                    'invoice_date': product.VEC_DIS_DT,
+                    'dealer_id': product.KUNNR,
+                    'unique_service_coupon': product.UCN_NO,
+                    'valid_days': product.DAYS_LIMIT_TO,
+                    'valid_kms': product.KMS_TO,
+                    'service_type': product.SERVICE_TYPE,
+                    'coupon_status': settings.DEFAULT_COUPON_STATUS,
+                })
+            response = save_to_db(
+                feed_type='dispatch', data_source=product_dispatch_list)
             return json.dumps(get_response(response))
         except Exception as ex:
-            logger.error("ProductDispatchService: {0}  Error on Validating ".format(ex))
+            logger.error(
+                "ProductDispatchService: {0}  Error on Validating ".format(ex))
             logger.debug("ProductDispatchService: Object List is "
                          .format(ObjectList.ProductDispatchData))
             return failed
@@ -174,81 +262,87 @@ class ProductDispatchService(ServiceBase):
 
 def get_response(response):
     failed_feed = response[1]['Failed']
-    return  failed if failed_feed > 0 else success
+    return failed if failed_feed > 0 else success
 
 
 class ProductPurchaseService(ServiceBase):
     __namespace__ = tns
-    @srpc(ProductPurchaseModelList,AuthenticationModel, _returns=Unicode)
+
+    @srpc(ProductPurchaseModelList, AuthenticationModel, _returns=Unicode)
     def postProductPurchase(ObjectList, Credential):
         try:
             product_purchase_list = []
             for product in ObjectList.ProductPurchaseData:
                 product_purchase_list.append({
-                        'vin' : product.CHASSIS,
-                        'sap_customer_id' : product.CUSTOMER_ID,
-                        'customer_phone_number' : utils.mobile_format(product.CUST_MOBILE),
-                        'customer_name' : product.CUSTOMER_NAME,
-                        'city' : product.CITY,
-                        'state' : product.STATE,
-                        'pin_no' : product.PIN_NO,
-                        'product_purchase_date' : product.VEH_SL_DT,
-                        'engine' : product.ENGINE
+                    'vin': product.CHASSIS,
+                    'sap_customer_id': product.CUSTOMER_ID,
+                    'customer_phone_number': utils.mobile_format(product.CUST_MOBILE),
+                    'customer_name': product.CUSTOMER_NAME,
+                    'city': product.CITY,
+                    'state': product.STATE,
+                    'pin_no': product.PIN_NO,
+                    'product_purchase_date': product.VEH_SL_DT,
+                    'engine': product.ENGINE
                 })
-            response = save_to_db(feed_type = 'purchase', data_source = product_purchase_list)
+            response = save_to_db(
+                feed_type='purchase', data_source=product_purchase_list)
             return json.dumps(get_response(response))
         except Exception as ex:
-            logger.error("ProductPurchaseService: {0}  Error on Validating ".format(ex))
-            logger.debug("ProductPurchaseService: Object List is "\
+            logger.error(
+                "ProductPurchaseService: {0}  Error on Validating ".format(ex))
+            logger.debug("ProductPurchaseService: Object List is "
                          .format(ObjectList.ProductPurchaseData))
-            return  failed
+            return failed
 
 
-def save_to_db(feed_type = None, data_source = []):
+def save_to_db(feed_type=None, data_source=[]):
     sap_obj = SAPFeed()
-    return sap_obj.import_to_db(feed_type = feed_type, data_source = data_source)
+    return sap_obj.import_to_db(feed_type=feed_type, data_source=data_source)
 
 
 def _on_method_call(ctx):
     if ctx.in_object is None:
         raise ArgumentError("Request doesn't contain data")
-    auth_obj = AuthenticationService(username = ctx.in_object.Credential.UserName, password = ctx.in_object.Credential.Password)
+    auth_obj = AuthenticationService(
+        username=ctx.in_object.Credential.UserName, password=ctx.in_object.Credential.Password)
     auth_obj.authenticate()
-     
+
 BrandService.event_manager.add_listener('method_call', _on_method_call)
 DealerService.event_manager.add_listener('method_call', _on_method_call)
-ProductDispatchService.event_manager.add_listener('method_call', _on_method_call)
-ProductPurchaseService.event_manager.add_listener('method_call', _on_method_call)
+ProductDispatchService.event_manager.add_listener(
+    'method_call', _on_method_call)
+ProductPurchaseService.event_manager.add_listener(
+    'method_call', _on_method_call)
 
 all_app = Application([BrandService, DealerService, ProductDispatchService, ProductPurchaseService],
-    tns=tns,
-    in_protocol=Soap11(validator='lxml'),
-    out_protocol=Soap11()
-)
+                      tns=tns,
+                      in_protocol=Soap11(validator='lxml'),
+                      out_protocol=Soap11()
+                      )
 
 brand_app = Application([BrandService],
-    tns=tns,
-    in_protocol=Soap11(validator='lxml'),
-    out_protocol=Soap11()
-)
+                        tns=tns,
+                        in_protocol=Soap11(validator='lxml'),
+                        out_protocol=Soap11()
+                        )
 
 dealer_app = Application([DealerService],
-    tns=tns,
-    in_protocol=Soap11(validator='lxml'),
-    out_protocol=Soap11()
-)
+                         tns=tns,
+                         in_protocol=Soap11(validator='lxml'),
+                         out_protocol=Soap11()
+                         )
 
 dispatch_app = Application([ProductDispatchService],
-    tns=tns,
-    in_protocol=Soap11(validator='lxml'),
-    out_protocol=Soap11()
-)
+                           tns=tns,
+                           in_protocol=Soap11(validator='lxml'),
+                           out_protocol=Soap11()
+                           )
 
 purchase_app = Application([ProductPurchaseService],
-    tns=tns,
-    in_protocol=Soap11(validator='lxml'),
-    out_protocol=Soap11()
-)
+                           tns=tns,
+                           in_protocol=Soap11(validator='lxml'),
+                           out_protocol=Soap11()
+                           )
 
 all_service = csrf_exempt(DjangoApplication(all_app))
 brand_service = csrf_exempt(DjangoApplication(brand_app))
