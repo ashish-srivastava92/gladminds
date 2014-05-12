ffc872ecf35fd25d35468be349cc78df44e42244
Shubham <shubham.lakhiwal@hashedin.com>
2014-05-12 16:33:40 +0530

    GM-132: Upload jobCard file to S3 and store the address. Write test cases for exceptions.:: Test cases have been written.

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from datetime import datetime, timedelta
-from gladminds.utils import get_sa_list
+from gladminds.utils import get_sa_list, get_coupon_info, get_customer_info
+from unit.base_unit import RequestObject, GladmindsUnitTestCase
 
 class TestAssertWorks(TestCase):
 
@@ -10,7 +11,7 @@ class TestAssertWorks(TestCase):
     def test_assert_true(self):
         self.assertTrue(100 > 1, '100 > 1')
 
-class TestUtils(TestCase):
+class TestUtils(GladmindsUnitTestCase):
     
     def setUp(self):
         super(TestUtils, self).setUp()
@@ -22,28 +23,20 @@ class TestUtils(TestCase):
                                            , customer_phone_number=customer_obj, sap_customer_id='SAP001')
         service_advisor = self.get_service_advisor_obj(service_advisor_id='SA001Test', name='UMOTO', phone_number='+914444861111')
         self.get_dealer_service_advisor_obj(dealer_id=dealer_obj, service_advisor_id=service_advisor, status='Y')
-        service_advisor1 = self.get_service_advisor_obj(service_advisor_id='SA002Test', name='UMOTOR', phone_number='+919999999999')
-        self.get_dealer_service_advisor_obj(dealer_id=dealer_obj, service_advisor_id=service_advisor1, status='Y')
         self.get_coupon_obj(unique_service_coupon='COUPON005', vin=product_obj, valid_days=30, valid_kms=500\
-                            , service_type=1, status=1, mark_expired_on=datetime.now() - timedelta(days=2)\
+                            , service_type=1, status=4, mark_expired_on=datetime.now() - timedelta(days=2)\
                             , actual_service_date=datetime.now() - timedelta(days=20), extended_date=datetime.now() - timedelta(days=2))
-        customer_obj1 = self.get_customer_obj(phone_number='8888888')
-        product_obj1 = self.get_product_obj(vin="VINXXX002", product_type=product_type_obj, dealer_id=dealer_obj\
-                                           , customer_phone_number=customer_obj1, sap_customer_id='SAP002')
-        self.get_coupon_obj(unique_service_coupon='COUPON004', vin=product_obj1, valid_days=30, valid_kms=500\
-                            , service_type=1, status=1, mark_expired_on=datetime.now() - timedelta(days=2)\
-                            , actual_service_date=datetime.now() - timedelta(days=20), extended_date=datetime.now() - timedelta(days=2))
-        self.get_coupon_obj(unique_service_coupon='COUPON006', vin=product_obj, valid_days=30, valid_kms=3000\
-                            , service_type=2, status=1, mark_expired_on=datetime.now() + timedelta(days=30)\
-                            , actual_service_date=datetime.now() - timedelta(days=20), extended_date=datetime.now() + timedelta(days=30))
-        self.get_coupon_obj(unique_service_coupon='COUPON007', vin=product_obj, valid_days=30, valid_kms=6000\
-                            , service_type=3, status=1, mark_expired_on=datetime.now() + timedelta(days=60)\
-                            , actual_service_date=datetime.now() - timedelta(days=20), extended_date=datetime.now() + timedelta(days=60))
-    
+        self.get_message_template(template_key='SEND_CUSTOMER_VALID_COUPON'\
+                                  ,template='Service Type {service_type}. UCN {coupon}.', description='Desc')
+
     def test_get_sa_list(self):
-        request = {'user': 'DEALER001'}
-        sa_list = get_sa_list()
-    
+        request = RequestObject(user='DEALER001', data={'customerId': 'SAP001', 'vin': 'VINXXX001'}, file={'jobCard': ''})
+        sa_list = get_sa_list(request)
+        self.assertEqual(len(sa_list), 1)
+        coupon_info=get_coupon_info(request)
+        self.assertEqual(coupon_info.keys(), ['status', 'message'])
+        customer = get_customer_info(request)
+        self.assertEquals(customer.keys(), ['customer_phone', 'customer_id'])
     
     
     
