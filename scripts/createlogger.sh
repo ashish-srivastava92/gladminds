e14019b98b85efffda59461fe7868e8c1cc6adf4
Sourabh Gupta <sourabh5588gupta@gmail.com>
2014-05-13 18:47:43 +0530

    add logger files

--- scripts/createlogger.sh
+++ scripts/createlogger.sh
@@ -1,1 +1,5 @@
 mkdir -p  /var/log/gladminds/app/
+echo "" > /var/log/gladminds/app/gladminds.log
+echo "" > /var/log/gladminds/app/afterbuy.log
+echo "" > /var/log/gladminds/app/test_case.log
+chmod -R a+x  /var/log/gladminds/app/
