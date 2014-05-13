be2a632cf79d9fdfe2e72d288016c8e421c45fc4
Sourabh Gupta <sourabh5588gupta@gmail.com>
2014-05-13 20:35:22 +0530

    updated location of damo file and revereted the changes of test feed data

---
+++ etc/demo_data/upload_demo_data.py
@@ -0,0 +1,15 @@
+from httplib2 import Http
+
+def main():
+    h = Http()
+
+    POST_URL = 'http://api-qa.gladmindsplatform.co/api/v1/bajaj/feed/'
+    # POST_URL = 'http://127.0.0.1:8000/api/v1/bajaj/feed/'
+    files = ['etc/demo_data/service_advisor_feed.xml', 'etc/demo_data/product_dispatch_feed.xml', 'etc/demo_data/product_purchase_feed.xml']
+    for file in files:
+        data = open(file, 'r')
+        response = h.request(POST_URL, method="POST", body=str(data.read()))
+        data.close()
+
+if __name__ == '__main__':
+    main()
