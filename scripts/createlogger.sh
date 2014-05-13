9cb7918963facead86251a24490211ffc0805b4c
Sourabh Gupta <sourabh5588gupta@gmail.com>
2014-05-13 20:43:52 +0530

    updated permission of logger files in createlogger for ebs build

--- scripts/createlogger.sh
+++ scripts/createlogger.sh
@@ -2,4 +2,4 @@ mkdir -p  /var/log/gladminds/app/
 echo "" > /var/log/gladminds/app/gladminds.log
 echo "" > /var/log/gladminds/app/afterbuy.log
 echo "" > /var/log/gladminds/app/test_case.log
-chmod -R a+x  /var/log/gladminds/app/
+chmod -R o+rwx  /var/log/gladminds/app/
