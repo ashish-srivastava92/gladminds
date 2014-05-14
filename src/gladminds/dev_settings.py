23fe6f40bad7638ae0f1c93ed8251f6ae785d746
naureen razi <naureen.razi@hashedin.com>
2014-05-14 13:40:09 +0530

    reverted dev setting chnages committed earlier

--- src/gladminds/dev_settings.py
+++ src/gladminds/dev_settings.py
@@ -21,18 +21,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'gladminds.db',                  # Or path to database file if using sqlite3.
-#         # The following settings are not used with sqlite3:
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-#         'PORT': '',                      # Set to empty string for default.
-#     }
-# }
-
 TNS = "http://api.gladmindsplatform.co/api/v1/bajaj/feed/"
 
 BROKER_URL= 'redis://localhost:6379'
