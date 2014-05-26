f40222f8ccbd9012919fefb7b4071fd7d79b0fc9
Sourabh Gupta <sourabh5588gupta@gmail.com>
2014-05-26 16:36:37 +0530

    Added after buy test cases fiel

---
+++ tests/integration/test_afterbuy.py
@@ -0,0 +1,13 @@
+''' Test Case for testing out the Afterbuy Api
+'''
+
+from integration.base_integration import GladmindsResourceTestCase
+
+
+class TestAfterbuy(GladmindsResourceTestCase):
+
+    def setUp(self):
+        pass
+
+    def test_user_registration(self):
+        pass
