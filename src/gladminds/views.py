a572a490d3fa7580975c092f05d78128197ff966
cppatels8 <cppatels8@gmail.com>
2014-07-31 20:29:21 +0530

    Issue resolved - GM-237

--- src/gladminds/views.py
+++ src/gladminds/views.py
@@ -1,6 +1,7 @@
 import logging
 import json
 import random
+import datetime
 
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
@@ -198,6 +199,13 @@ def exceptions(request, exception=None):
             return HttpResponseBadRequest()
     else:
         return HttpResponseBadRequest()
+
+def subtract_dates(start_date, end_date):    
+    start_date = start_date.strftime("%Y-%m-%d")
+    end_date = end_date.strftime("%Y-%m-%d")
+    start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+    end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d") 
+    return start_date - end_date
     
 UPDATE_FAIL = 'Some error occurred, try again later.'
 UPDATE_SUCCESS = 'Customer has been registered with ID: '
@@ -207,6 +215,16 @@ def register_customer(request, group=None):
     product_obj = common.ProductData.objects.filter(vin=post_data['customer-vin'])
     temp_customer_id = TEMP_ID_PREFIX + str(random.randint(10**5, 10**6))
     data_source.append(utils.create_feed_data(post_data, product_obj[0], temp_customer_id))
+    
+    check_with_invoice_date = subtract_dates(data_source[0]['product_purchase_date'], product_obj[0].invoice_date)    
+    check_with_today_date = subtract_dates(data_source[0]['product_purchase_date'], datetime.datetime.now())
+    
+    if check_with_invoice_date.days < 0 or check_with_today_date.days > 0:
+        message = "Product purchase date should be between {0} and {1}".\
+                format((product_obj[0].invoice_date).strftime("%d-%m-%Y"),(datetime.datetime.now()).strftime("%d-%m-%Y"))
+        logger.info('{0} Entered date is: {1}'.format(message, str(data_source[0]['product_purchase_date'])))
+        return json.dumps({"message": message})
+         
     try:
         customer_obj = common.CustomerTempRegistration(product_data=product_obj[0], 
                                                        new_customer_name = data_source[0]['customer_name'],
