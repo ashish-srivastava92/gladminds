3551e0fb19b79ff852c92a312b65aa75a2b13909
priyanka66 <priyanka@hashedin.com>
2014-08-13 13:37:52 +0530

    Added functionality to send mail to assignee based on the status of feedback  and changed the feedback model

--- src/gladminds/signals.py
+++ src/gladminds/signals.py
@@ -1,7 +1,13 @@
-from gladminds.mail import send_email_to_assignee
+from gladminds.mail import send_email_to_assignee, send_status_mail_to_assignee
+
 
 def send_sms(sender, **kwargs):
     instance_object = kwargs['instance']
     if instance_object.assign_to:
         send_email_to_assignee(instance_object)
-        
+        phone_number = instance_object.assign_to.phone_number
+        print phone_number        
+    if instance_object.status=='Resolved':
+            send_status_mail_to_assignee(instance_object)
+
+
