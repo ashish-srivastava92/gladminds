de3484b1ac955ece7423289c635140ad8a33fd54
priyanka66 <priyanka@hashedin.com>
2014-08-25 12:54:52 +0530

    Resolved merge conflicts

--- src/gladminds/utils.py
+++ src/gladminds/utils.py
@@ -313,5 +313,6 @@ def get_search_query_params(request, class_self):
         custom_search_enabled = True
     return custom_search_enabled
 
+import datetime
 def get_min_and_max_filter_date():
     return (datetime.date.today() - datetime.timedelta(6*365/12)).isoformat(), (datetime.date.today()).isoformat()
