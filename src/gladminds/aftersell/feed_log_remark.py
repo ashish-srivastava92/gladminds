c4f8e29ab7b984f8df071f97cc6835288861b4e7
priyanka66 <priyanka@hashedin.com>
2014-09-02 14:57:13 +0530

    Fixed few pylint errors in all these files

--- src/gladminds/aftersell/feed_log_remark.py
+++ src/gladminds/aftersell/feed_log_remark.py
@@ -31,37 +31,31 @@ class FeedLogWithRemark():
 
         path = ""
         if self.failed_feeds and settings.FEED_FAILURE_MAIL_ENABLED:
-            send_report_mail_for_feed_failure(remarks=remarks, 
-                                              feed_type = self.feed_type)
-        
+            send_report_mail_for_feed_failure(remarks=remarks,feed_type=self.feed_type)
             try:
                 path = self.upload_file(remarks)
             except Exception as ex :
                 logger.error("Feed file not updated on s3 : %s" % ex)
-        
+
         feed_log(feed_type=self.feed_type, total_data_count=self.total_feeds,
-              failed_data_count=self.failed_feeds,
-              success_data_count=success_data_count, status=self.status,
-              action=self.action, remarks=remarks, file_location=path)
-    
+                 failed_data_count=self.failed_feeds,
+                 success_data_count=success_data_count, status=self.status,
+                 action=self.action, remarks=remarks, file_location=path)
+
     def upload_file(self, remarks):
         file_name = self.get_file_name()
-        
         file_obj = open(file_name, 'w')
         file_obj.write(remarks)
         file_obj.close()
         file_obj = open(file_name, 'r')
         destination = settings.FEED_FAILURE_DIR.format('bajaj')
-        
-        path = uploadFileToS3(destination=destination, file_obj=file_obj, 
-                  bucket=settings.FEED_FAILURE_BUCKET, file_mimetype='text/plain',
-                  logger_msg="Feed File Failure Uploaded")
+        path = uploadFileToS3(destination=destination, file_obj=file_obj,
+                              bucket=settings.FEED_FAILURE_BUCKET, file_mimetype='text/plain',
+                              logger_msg="Feed File Failure Uploaded")
         file_obj.close()
-        
         os.remove("{0}/{1}".format(settings.BASE_DIR, file_name))
-        
         return path
-    
+
     def get_file_name(self):
         filename_suffix = str(uuid.uuid4())
         filename_prefix = '_'.join(self.feed_type.split(" "))
