b51f769284ef74a8420bc2c9fa2af2184d26d310
cppatels8 <cppatels8@gmail.com>
2014-09-11 18:52:59 +0530

    Added fixes for search issue

--- src/gladminds/templatetags/admintags.py
+++ src/gladminds/templatetags/admintags.py
@@ -6,4 +6,6 @@ register = template.Library()
 @register.filter(name="get_coupon_status")
 def get_coupon_status(value):
     """Get coupons on basis of status that will be int returned value string"""
-    return STATUS_CHOICES[int(value)-1][1]
+    coupon_status = dict((str(k), v) for k, v in dict(STATUS_CHOICES).items())
+    return coupon_status.get(str(value), value)
+    
