5407b4b0615492a793462cace2202c107b5ebdcf
somit <somitbit@gmail.com>
2014-09-19 16:00:44 +0530

    removed unused imports

--- src/gladminds/urls.py
+++ src/gladminds/urls.py
@@ -5,7 +5,7 @@ from gladminds.resource import resources
 from gladminds.sqs_tasks import _tasks_map
 from gladminds.taskqueue import SqsHandler
 from gladminds.afterbuy import api_resource
-from gladminds.apis import audit_api, product_api
+from gladminds.apis import audit_api
 from gladminds.apis import user_apis
 
 # Uncomment the next two lines to enable the admin:
