6b982aaa75b93638e4d40e9cce2fb6bad4eab682
priyanka66 <priyanka@hashedin.com>
2014-09-24 15:20:00 +0530

    Fixed review comments

--- tests/integration/test_gladmind_apis.py
+++ tests/integration/test_gladmind_apis.py
@@ -1,18 +1,15 @@
-import requests
 from django.contrib.auth.models import User
-    
 from django.test.client import Client
 from tastypie.test import ResourceTestCase, TestApiClient
-from django.test.testcases import TestCase
-import json
 from provider.oauth2.models import AccessToken
 from provider.oauth2.models import Client as auth_client
+from test_constants import GM_USER, GM_PRODUCTS, GM_COUPONS
 client = TestApiClient()
 djangoClient=Client()
 
 
 class GladMindsApiTests(ResourceTestCase):
-    
+
     def setUp(self):
         super(GladMindsApiTests, self).setUp()
         self.access_token = 'testaccesstoken'
@@ -21,99 +18,20 @@ class GladMindsApiTests(ResourceTestCase):
         secret_cli.save()
         access = AccessToken(user=user, token=self.access_token, client=secret_cli)
         access.save()
-        self.gm_user = {
-                  "customer_name": "test_user",
-                  "isActive": True,
-                  "phone_number": "1234567890",
-                  "user": {
-                           "phone_number": "999999999",
-                           "user": {
-                                    "email": "",
-                                    "first_name": "",
-                                    "last_name": "",
-                                    "username": "ppa",
-                                    "password" :"123"
-                                    }
-                           }
-                  }
-        
-            
-        self.gm_products = {
-                            "customer_product_number": None,
-                            "engine": None,
-                            "insurance_loc": None,
-                            "insurance_yrs": None,      
-                            "invoice_loc": None,        
-                            "isActive": True,
-                            "order": 0,
-                            "product_type": {
-                                             "brand": {
-                                                          "brand_id": "Bajaaaaj",
-                                                          "brand_image_loc": " ",
-                                                          "brand_name": "Bajaj",
-                                                          "isActive": True
-                                                          },
-                                             "isActive": True,
-                                             "order": 0,
-                                             "product_image_loc": None,
-                                             "product_name": "3700DHPP",
-                                             "product_type": "3700DHPP",                  
-                                             "warranty_email": None,                      
-                                             "warranty_phone": None                       
-                                             },                                              
-                            "purchased_from": None,
-                            "sap_customer_id": None,    
-                            "seller_email": None,       
-                            "seller_phone": None,       
-                            "veh_reg_no": None,         
-                            "vin": 22,                  
-                            "warranty_loc": None,       
-                            "warranty_yrs": None
-                            }
-        self.coupons = {
-                        "order": 0,
-                        "service_type": 2,
-                        "status": 1,
-                        "unique_service_coupon": "1",
-                        "valid_days": 260,
-                        "valid_kms": 8000,
-                        "vin": {
-                                "created_on": "2014-04-25T15:56:34",
-                                "invoice_date": "2013-04-06T00:00:00",
-                                "isActive": True,               
-                                "last_modified": "2014-04-25T15:56:34",
-                                "order": 0,                     
-                                "product_type": {
-                                                 "brand": {
-                                                           "brand_id": "Bajaj",
-                                                           "brand_image_loc": " ",
-                                                           "brand_name": "Bajaj",
-                                                           "isActive": True                                           
-                                                           },
-                                                 "isActive": True,
-                                                 "order": 0,
-                                                 "product_image_loc": "",
-                                                 "product_name": "3700DHFS",
-                                                 "product_type": "3700DHFS",
-                                                 "product_type_id": 101959
-                                                 },
-                                "vin": "22"
-                                }
-                        }                       
 
     def add_a_gmuser(self):
         uri = '/v1/gmusers/'
-        resp = self.api_client.post(uri, format='json', data=self.gm_user)
+        resp = self.api_client.post(uri, format='json', data=GM_USER)
         return resp
-    
+
     def add_a_product(self):
         uri = '/v1/products/'
-        resp = self.api_client.post(uri, format='json', data=self.gm_products)
+        resp = self.api_client.post(uri, format='json', data=GM_PRODUCTS)
         return resp
-    
+
     def add_a_coupon(self):
         uri = '/v1/coupons/'
-        resp = self.api_client.post(uri, format='json', data=self.coupons)
+        resp = self.api_client.post(uri, format='json', data=GM_COUPONS)
         return resp
 
     def test_create_a_gmuser(self):
@@ -146,7 +64,7 @@ class GladMindsApiTests(ResourceTestCase):
         self.assertEqual(len(self.deserialize(resp)['objects']), 1)
         resp = self.api_client.delete('/v1/gmusers/1/', format='json')
         self.assertEquals(resp.status_code,204)
-    
+
     def test_create_a_product(self):
         resp = self.add_a_product()
         self.assertEquals(resp.status_code,201)
@@ -158,7 +76,7 @@ class GladMindsApiTests(ResourceTestCase):
         self.assertEquals(resp.status_code,200)
         self.assertEqual(self.deserialize(resp)['vin'], "22")
         self.assertEqual(len(self.deserialize(resp)), 23)
-        
+
     def test_update_a_product(self):
         resp = self.add_a_product()
         self.assertEquals(resp.status_code,201)
@@ -168,7 +86,7 @@ class GladMindsApiTests(ResourceTestCase):
         resp = self.api_client.get('/v1/products/1/', format='json')
         self.assertEquals(resp.status_code, 200)
         self.assertEqual(self.deserialize(resp)['vin'], "11")
-    
+
     def test_delete_product(self):
         resp = self.add_a_product()
         self.assertEquals(resp.status_code,201)
@@ -177,7 +95,7 @@ class GladMindsApiTests(ResourceTestCase):
         self.assertEqual(len(self.deserialize(resp)['objects']), 1)
         resp = self.api_client.delete('/v1/products/1/', format='json')
         self.assertEquals(resp.status_code,204)
-        
+
     def test_create_a_coupon(self):
         resp = self.add_a_coupon()
         self.assertEquals(resp.status_code,201)
@@ -189,7 +107,7 @@ class GladMindsApiTests(ResourceTestCase):
         self.assertEquals(resp.status_code,200)
         self.assertEqual(self.deserialize(resp)['unique_service_coupon'], '1')
         self.assertEqual(len(self.deserialize(resp)), 18)
-        
+
     def test_update_a_coupon(self):
         resp = self.add_a_coupon()
         self.assertEquals(resp.status_code,201)
@@ -200,7 +118,7 @@ class GladMindsApiTests(ResourceTestCase):
         resp = self.api_client.get('/v1/coupons/1/', format='json')
         self.assertEquals(resp.status_code, 200)
         self.assertEqual(self.deserialize(resp)['unique_service_coupon'], '2')
-    
+
     def test_delete_coupon(self):
         resp = self.add_a_coupon()
         self.assertEquals(resp.status_code,201)
@@ -209,4 +127,3 @@ class GladMindsApiTests(ResourceTestCase):
         self.assertEqual(len(self.deserialize(resp)['objects']), 1)
         resp = self.api_client.delete('/v1/coupons/1/', format='json')
         self.assertEquals(resp.status_code,204)
-    
