ca71b507ff8ca9891e20728360cba5e292c21f7d
cppatels8 <cppatels8@gmail.com>
2014-10-16 14:51:17 +0530

    Added custom static file loader

--- src/gladminds/settings.py
+++ src/gladminds/settings.py
@@ -182,6 +182,7 @@ STATICFILES_DIRS = (
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
+    'gladminds.core.custom_staticfiles_loader.FileSystemFinder',                   
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
