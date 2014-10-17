9bd8e5e0be75685e82e9d8e1757ab7bccf146bd8
cppatels8 <cppatels8@gmail.com>
2014-10-17 19:43:23 +0530

    Fixed kap issue

--- src/gladminds/middleware.py
+++ src/gladminds/middleware.py
@@ -18,15 +18,13 @@ and reply through the same platform
 class GladmindsMessageMiddleware(object):    
     #If the request is come on message APIs, then based on the IP we set sms client
     def process_request(self, request, **kwargs):
-        logger.info(request.POST)
-        logger.info(request)
-        logger.info(request.GET)
         source_client = request.POST.get('source', None)
+        logger.info('Client is {0}'.format(source_client))
         if source_client:
             SMS_CLIENT.value = 'KAP'
         else:
             SMS_CLIENT.value = 'AIRTEL'
-        logger.info('Client is {1}'.format(SMS_CLIENT.value))
+        logger.info('Client is {0}'.format(SMS_CLIENT.value))
         
 
 """
