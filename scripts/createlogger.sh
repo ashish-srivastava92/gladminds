273eedb2812a0c532ba529f79ad48b6d183e3d23
Naureen Razi <naureen.razi@hahsedin.com>
2014-10-20 14:44:34 +0530

    reverted the changes for log location

--- scripts/createlogger.sh
+++ scripts/createlogger.sh
@@ -1,7 +1,6 @@
-chmod -R 744 /var/log/httpd/
-mkdir -p  /var/log/httpd/gladminds/
-echo "" >> /var/log/httpd/gladminds/sql.log
-echo "" >> /var/log/httpd/gladminds/gladminds.log
-echo "" >> /var/log/httpd/gladminds/afterbuy.log
-echo "" >> /var/log/httpd/gladminds/test_case.log
-chmod -R o+rwx /var/log/httpd/gladminds/
+mkdir -p  /var/log/gladminds/
+echo "" >> /var/log/gladminds/sql.log
+echo "" >> /var/log/gladminds/gladminds.log
+echo "" >> /var/log/gladminds/afterbuy.log
+echo "" >> /var/log/gladminds/test_case.log
+chmod -R o+rwx /var/log/gladminds/
