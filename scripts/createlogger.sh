51734fc4014595d13c67da51af49c0d77c9b784d
Naureen Razi <naureen.razi@hahsedin.com>
2014-10-20 13:47:41 +0530

    updated the permisions on httpd file

--- scripts/createlogger.sh
+++ scripts/createlogger.sh
@@ -1,4 +1,4 @@
-chmod -R 704 /var/log/httpd/
+chmod -R 744 /var/log/httpd/
 mkdir -p  /var/log/httpd/gladminds/
 echo "" >> /var/log/httpd/gladminds/sql.log
 echo "" >> /var/log/httpd/gladminds/gladminds.log
