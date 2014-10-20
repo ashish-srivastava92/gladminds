4338deb69d4d231425b8023c189046bee730d458
srv.sngh <saurav.kumar@hashedin.com>
2014-10-21 00:57:41 +0530

    added user apis

--- src/gladminds/afterbuy/apis/user_apis.py
+++ src/gladminds/afterbuy/apis/user_apis.py
@@ -33,27 +33,25 @@ class UserResources(CustomBaseResource):
             url(r"^(?P<resource_name>%s)/registration%s" % (self._meta.resource_name, trailing_slash()), self.wrap_view('save_user_details'), name="save_user_details"),
             url(r"^(?P<resource_name>%s)/authenticate/email-id%s" % (self._meta.resource_name, trailing_slash()), self.wrap_view('authenticate_user_email_id'), name="authenticate_user_email_id"),
             url(r"^(?P<resource_name>%s)/send-opt%s" % (self._meta.resource_name, trailing_slash()), self.wrap_view('authenticate_user_send_otp'), name="authenticate_user_send_otp"),
-            url(r"^(?P<resource_name>%s)/product/purchase-info%s" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_product_purchase_information'), name="get_product_purchase_information"),
-            url(r"^(?P<resource_name>%s)/product/warranty%s" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_product_warranty'), name="get_product_warranty"),
-            url(r"^(?P<resource_name>%s)/product/insurance%s" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_product_insurance'), name="get_product_insurance"),
+            url(r"^(?P<resource_name>%s)/forgot-password%s" % (self._meta.resource_name, trailing_slash()), self.wrap_view('change_user_password'), name="change_user_password"),
+            url(r"^(?P<resource_name>%s)/details%s" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_user_details'), name="get_user_details"),
             url(r"^(?P<resource_name>%s)/product/info%s" % (self._meta.resource_name, trailing_slash()), self.wrap_view('dispatch_dict'), name="api_dispatch_dict"),
-            url(r"^(?P<resource_name>%s)/notification/count%s" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_notification_count'), name="get_notification_count"),
-            url(r"^(?P<resource_name>%s)/notification/list%s" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_notification_list'), name="get_notification_list"),
-            url(r"^(?P<resource_name>%s)/product/spares%s" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_spares_list'), name="get_spares_list"),
-            url(r"^(?P<resource_name>%s)/phone-details%s" % (self._meta.resource_name, trailing_slash()), self.wrap_view('save_user_phone_details'), name="save_user_phone_details"),
         ]
 
     def save_user_details(self, request, **kwargs):
-        data = request.POST
-        if not data:
-            return HttpBadRequest("phone_number is required.")
+        phone_number = request.POST.get('phone_number')
+        email_id = request.POST.get('email_id')
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        if not phone_number or not email_id or not name:
+            return HttpBadRequest("phone_number, username and password required.")
         try:
-            phone_number = mobile_format(data['phone_number'])
-            create_user = User(username=data['name'], password=data['password'], email=data['email_id'])
+            phone_number = mobile_format(phone_number)
+            create_user = User(username=name, password=password, email=email_id)
             create_user.save()
             try:
                 user_details = afterbuy_common.Consumer.objects.get(phone_number=phone_number)
-                data = data={'status':0, 'message':'already registered'}
+                data = {'status': 0, 'message': 'already registered'}
             except:
                 customer_id = utils.generate_unique_customer_id()
                 user_register = afterbuy_common.Consumer(user=create_user, phone_number=phone_number, consumer_id=customer_id)
@@ -62,7 +60,7 @@ class UserResources(CustomBaseResource):
         except Exception as ex:
             log_message = "unable to save details :{0}".format(ex)
             logger.info(log_message)
-            data={'status':0, 'message':log_message}
+            data = {'status': 0, 'message': log_message}
         return HttpResponse(json.dumps(data), content_type="application/json")
 
     def dispatch_dict(self, request, **kwargs):
@@ -81,7 +79,7 @@ class UserResources(CustomBaseResource):
             user_info = afterbuy_common.Consumer.objects.get(phone_number=phone_number)
             product_info = afterbuy_common.UserProduct.objects.filter(consumer=user_info)
             if not product_info:
-                data={'status':0, 'message':"No product exist."}
+                data = {'status': 0, 'message': "No product exist."}
                 return HttpResponse(json.dumps(data), content_type="application/json")
             else:
                 for product_object in map(model_to_dict, product_info):
@@ -92,11 +90,11 @@ class UserResources(CustomBaseResource):
         return HttpResponse(json.dumps(resp))
 
     def authenticate_user_email_id(self, request, **kwargs):
-        data = request.POST
-        if not data:
+        email_id = request.POST.get('email_id')
+        if not email_id:
             return HttpBadRequest("email id is required")
         try:
-            user_email_id = User.objects.filter(email=data['email_id'])
+            user_email_id = User.objects.filter(email=email_id)
             if len(user_email_id) > 0:
                 data = {'status': 1, 'message': 'authenticated email_id'}
             else:
@@ -108,11 +106,11 @@ class UserResources(CustomBaseResource):
         return HttpResponse(json.dumps(data), content_type="application/json")
 
     def authenticate_user_send_otp(self, request, **kwargs):
-        data = request.POST
-        if not data:
+        phone_number = request.POST.get('phone_number')
+        if not phone_number:
             return HttpBadRequest("email id is required")
         try:
-            phone_number = data['phone_number']
+            phone_number = phone_number
             logger.info('OTP request received. Mobile: {0}'.format(phone_number))
             user = afterbuy_common.Consumer.objects.filter(phone_number=mobile_format(phone_number))[0]
             token = afterbuy_utils.get_token(user, phone_number)
@@ -128,7 +126,45 @@ class UserResources(CustomBaseResource):
                 sent_otp_email(data=token, receiver=user.email, subject='Your OTP')
                 data = {'status': 1, 'message': "OTP sent_successfully"}
         except Exception as ex:
-            print "ex",ex
             logger.error('Invalid details, mobile {0}'.format(request.POST.get('phone_number', '')))
             data = {'status': 0, 'message': "inavlid phone_number"}
         return HttpResponse(json.dumps(data), content_type="application/json")
+
+    def change_user_password(self, request, **kwargs):
+        phone_number = request.POST.get('phone_number')
+        password = request.POST.get('password')
+        if not phone_number and not password:
+            return HttpBadRequest("mobile and password required")
+        try:
+            phone_number = phone_number
+            consumer = afterbuy_common.Consumer.objects.filter(phone_number=mobile_format(phone_number))[0]
+            user = User.objects.get(id=consumer.user_id)
+            user.password = password
+            user.save()
+            data = {'status': 1, 'message': "password updated successfully"}
+        except Exception as ex:
+            logger.error('Invalid details, mobile {0}'.format(request.POST.get('phone_number', '')))
+            data = {'status': 0, 'message': "inavlid phone_number"}
+        return HttpResponse(json.dumps(data), content_type="application/json")
+
+    def get_user_details(self, request, **kwargs):
+        mobile = request.GET.get('phone_number')
+        cosumer_data = {}
+        if not mobile:
+            return HttpBadRequest("Phone Number is required.")
+        try:
+            consumer_obj = afterbuy_common.Consumer.objects.filter(phone_number=mobile_format(mobile))[0]
+            cosumer_data['username'] = consumer_obj.user.username
+            cosumer_data['email'] = consumer_obj.user.email
+            cosumer_data['phone_number'] = consumer_obj.phone_number
+            cosumer_data['image_url'] = consumer_obj.image_url
+            cosumer_data['address'] = consumer_obj.address
+            cosumer_data['state'] = consumer_obj.state
+            cosumer_data['country'] = consumer_obj.country
+            cosumer_data['date_of_birth'] = consumer_obj.date_of_birth
+            cosumer_data['accepted_terms'] = consumer_obj.accepted_terms
+            cosumer_data['tshirt_size'] = consumer_obj.tshirt_size
+        except Exception as ex:
+            logger.info("[Exception get_user_product_information]:{0}".format(ex))
+            return HttpBadRequest("Not a registered number")
+        return HttpResponse(json.dumps(cosumer_data), content_type="application/json")
