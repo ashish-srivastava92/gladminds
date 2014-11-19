3933f34806a0a27309679f717eadec955dfd1419
Naureen Razi <naureen.razi@hahsedin.com>
2014-11-19 20:38:10 +0530

    updated migration script

--- scripts/migrate_ucn_recovery.py
+++ scripts/migrate_ucn_recovery.py
@@ -4,44 +4,45 @@ import os
 from multiprocessing.dummy import Pool
 from datetime import datetime
 
-DB_HOST = os.environ.get('DB_HOST')
-DB_USER = os.environ.get('DB_USER')
-DB_PASSWORD = os.environ.get('DB_PASSWORD')
+POOL = Pool(50)
+DB_HOST = os.environ.get('DB_HOST', 'localhost')
+DB_USER = os.environ.get('DB_USER', 'root')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'gladminds')
+MIGRATE_DB = os.environ.get('MIGRATE_DB','gladmindsdb')
+OFFSET = int(os.environ.get('OFFSET',0))
+TOTAL_START_TIME = time.time()
 
-MIGRATE_DB = os.environ.get('MIGRATE_DB','gladminds')
-db_old = MySQLdb.connect(host=DB_HOST, # your host, usually localhost
+
+DB_OLD = MySQLdb.connect(host=DB_HOST, # your host, usually localhost
                      user=DB_USER, # your username
                       passwd=DB_PASSWORD, # your password
                       db=MIGRATE_DB) # name of the data base
 
-cur_old = db_old.cursor() 
+CUR_OLD = DB_OLD.cursor() 
 
-db_new = MySQLdb.connect(host=DB_HOST, # your host, usually localhost
+''' To migrate UCN RECOVERY DATA'''
+
+CUR_OLD.execute("SELECT c.*, a.username FROM aftersell_ucnrecovery as u, auth_user as a where u.user_id=a.id")
+ucnrecover_data = CUR_OLD.fetchall()
+
+DB_OLD.close()
+
+def process_query_ucnrecover(data):
+    db_new = MySQLdb.connect(host=DB_HOST, # your host, usually localhost
                      user=DB_USER, # your username
                       passwd=DB_PASSWORD, # your password
                       db="bajaj") # name of the data base
 
-cur_new = db_new.cursor() 
-
-''' To migrate UCN RECOVERY DATA'''
-
-cur_old.execute("SELECT c.*, a.username FROM aftersell_ucnrecovery as u, auth_user as a where u.user_id=a.id")
-ucnrecover_data = cur_old.fetchall()
-
-def process_query_ucnrecover(data):
-    print "--------------------coupons--------------", data.get('unique_service_coupon')
+    cur_new = db_new.cursor() 
     try:
         today = datetime.now()
-        print "-------------- dealer", data.get('username')
         query = "select id from auth_user where username  = %(username)s"
         cur_new.execute(query, {'username': data.get('username')})
         dealer = cur_new.fetchall()[0]
-        print "fetched dealer", dealer
         
         query2 = "select * from bajaj_userprofile where user_id  = %(user_id)s"
         cur_new.execute(query2, {'user_id': dealer[0]})
         dealer_pro = cur_new.fetchall()[0]
-        print "fetched dealer profile", dealer_pro
         
         cur_new.execute("INSERT INTO bajaj_ucnrecovery (id, created_date, modified_date,\
         reason, customer_id, file_location, unique_service_coupon, user_id) VALUES\
@@ -50,18 +51,16 @@ def process_query_ucnrecover(data):
         data.get('file_location'), data.get('unique_service_coupon'), dealer_pro[11]))
         
         db_new.commit()        
-        print "---------------DONE--------------------"
     except Exception as ex:
         db_new.rollback()
-        print "----------------------SOMETHING WENT WRONG--------------------", ex
+        print '[Error]:', data.get('id'), ex
+    db_new.close()
     
  
 def format_data_ucnrecover(ucnrecover_data):
     start_time = time.time()
-    pool = Pool(1)
     ucnrecovers=[]
     for data in ucnrecover_data:
-        print "----------------", data
         temp = {}
         temp['id'] = data[0]
         temp['reason'] = data[1]
@@ -72,9 +71,7 @@ def format_data_ucnrecover(ucnrecover_data):
         temp['unique_service_coupon'] = data[6]
         temp['username'] = data[7]
         ucnrecovers.append(temp)
-        print "------------------------", temp
-    print "------------------", ucnrecovers
-    pool.map(process_query_ucnrecover, ucnrecovers)
+    POOL.map(process_query_ucnrecover, ucnrecovers)
     end_time = time.time()
     print "..........Total TIME TAKEN.........", end_time-start_time
 
