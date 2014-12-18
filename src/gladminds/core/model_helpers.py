e7d87a27227a1433a99f88c749929ea23577aa45
somit <somitbit@gmail.com>
2014-12-18 20:54:50 +0530

    GM:729 Fixed mobile number formatting at alll places

--- src/gladminds/core/model_helpers.py
+++ src/gladminds/core/model_helpers.py
@@ -16,6 +16,17 @@ phone_re = re.compile(r'^\+?1?\d{9,15}$')
 validate_phone = RegexValidator(phone_re, _("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."), 'invalid')
 
 
+def get_phone_number_format(phone_number):
+    '''
+    This is used to format phone
+    '''
+    phone_number = phone_number.strip()
+    if phone_number.startswith('+91'):
+        phone_number = phone_number[3:]
+    numbers = re.compile('\d+(?:\d+)?')
+    return '+91' + ''.join(numbers.findall(phone_number))
+
+
 class PhoneInput(TextInput):
     input_type = 'phone'
 
@@ -26,6 +37,7 @@ class PhoneNoField(fields.CharField):
 
     def clean(self, value):
         value = self.to_python(value).strip()
+        value = get_phone_number_format(value)
         return super(PhoneNoField, self).clean(value)
 
 
