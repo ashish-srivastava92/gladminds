07c4cb8299ce0f399d6cd4f1f827579cecad31c7
priyanka66 <priyanka@hashedin.com>
2015-01-27 18:54:37 +0530

    GM-947 fixed review comments

--- src/gladminds/bajaj/services/service_desk/servicedesk_views.py
+++ src/gladminds/bajaj/services/service_desk/servicedesk_views.py
@@ -157,8 +157,7 @@ def modify_servicedesk_tickets(request, feedback_id):
     feedback_types = get_list_from_set(FEEDBACK_TYPE)
     root_cause = get_list_from_set(ROOT_CAUSE)
     feedback_obj = get_feedback(feedback_id, request.user)
-    servicedesk_users = get_servicedesk_users(designation=Roles.SDOWNERS)
-    servicedesk_manager = get_servicedesk_users(designation=Roles.SDMANAGERS)
+    servicedesk_users = get_servicedesk_users(designation=[Roles.SDOWNERS,Roles.SDMANAGERS] )
     comments = get_comments(feedback_id)
     
     if request.method == 'POST':
@@ -172,7 +171,6 @@ def modify_servicedesk_tickets(request, feedback_id):
                     "ROOT_CAUSE" : root_cause,\
                    "group": group_name[0].name,\
                    'servicedeskuser': servicedesk_users,\
-                   'servicedeskmanager':servicedesk_manager[0],\
                    'comments': comments,\
                    'user':request.user
                    })
