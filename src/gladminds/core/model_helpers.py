9ec59e70f7ad4c6865b522efe1b6033aa0df4e67
Anchit Gupta <anchit.gupta@hashedin.com>
2015-02-19 12:23:42 +0530

    GM-361 fixed review comments and made changes in model_helpers

--- src/gladminds/core/model_helpers.py
+++ src/gladminds/core/model_helpers.py
@@ -101,18 +101,24 @@ def validate_file(fieldfile_obj):
         megabyte_limit = settings.MAX_UPLOAD_FILE_SIZE
         if filesize > megabyte_limit*1024*1024:
             raise ValidationError("File size cannot exceed %sMB" % str(megabyte_limit))
-        
+
+
+def create_upload_path(folder, instance):
+    brand_path =  getattr(instance, '_state').__dict__['db']
+    return u'{0}/{1}/{2}'.format(settings.ENV,brand_path, folder)
+
+
 def set_service_training_material_path(instance,filename):
-    return u'{0}/{1}/training_material'.format(settings.ENV,settings.BRAND)
+    return create_upload_path('training_material', instance)
 
 def set_mechanic_pic_path(instance,filename):
-    return u'{0}/{1}/mechanics'.format(settings.ENV,settings.BRAND)
+    return create_upload_path('mechanics', instance)
 
 def set_product_catalog_pic_path(instance,filename):
-    return u'{0}/{1}/product'.format(settings.ENV,settings.BRAND)
+    return create_upload_path('product', instance)
 
 def set_redemption_request_pic_path(instance,filename):
-    return u'{0}/{1}/redemption'.format(settings.ENV,settings.BRAND)
+    return create_upload_path('redemption', instance)
 
 def set_welcome_kit_pic_path(instance,filename):
-    return u'{0}/{1}/welcome'.format(settings.ENV,settings.BRAND)
+    return create_upload_path('welcome', instance)
