6ffad9823f01a230e9e986c527e44929011229ff
priyanka66 <priyanka@hashedin.com>
2015-02-24 17:10:47 +0530

    GM-1033 Allow users to select departments while creating tickets for Daimler SD

--- src/gladminds/demo/urls.py
+++ src/gladminds/demo/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url, include
 from gladminds.demo.admin import brand_admin
 from gladminds.core import urls as core_urls
-
+from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^site-info/$', 'gladminds.demo.views.site_info', name='site_info'),
     url(r'', include(core_urls)),
     url(r'^', include(brand_admin.urls)),
+    (r'^admin/', include(admin.site.urls) )
 )
