8447df54f2a2c55110a3b9bcf4aa32e72a782e40
priyanka66 <priyanka@hashedin.com>
2015-03-02 18:50:03 +0530

    GM-1083 Allow dealer to add user

--- src/gladminds/settings.py
+++ src/gladminds/settings.py
@@ -655,7 +655,8 @@ HOME_URLS = {
              "demo" : {"SdManagers":[{"SERVICE DESK":"/aftersell/helpdesk"}],
                        "SdOwners" :[{"SERVICE DESK":"/aftersell/helpdesk"}],
                        "Dealers" :[{"SERVICE DESK":"/aftersell/helpdesk"}],
-                       "DealerAdmins":[{"SERVICE DESK":"/aftersell/helpdesk"}]
+                       "DealerAdmins":[{"SERVICE DESK":"/aftersell/helpdesk"},
+                                       {"ADD SERVICE DESK USER":"/add/servicedesk-user"}]
                        }
              }
 
