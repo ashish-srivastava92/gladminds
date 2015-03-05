36553c9c96d937ac96bae072e41e745825ec1a6a
somit <somitbit@gmail.com>
2015-03-05 13:44:23 +0530

    GM:1086 changed settings of QUEUE for staging

--- src/gladminds/staging_settings.py
+++ src/gladminds/staging_settings.py
@@ -141,7 +141,7 @@ AFTER_BUY_CONSTANTS = {
                        }
 
 ########################SQS Queue Name
-SQS_QUEUE_NAME = "gladminds-qa2"
+SQS_QUEUE_NAME = "gladminds-staging"
 ######################################
 UCN_RECOVERY_MAIL_DETAIL["subject"] = "GladMinds UCN Recovery Mail QA"
 VIN_DOES_NOT_EXIST_DETAIL["receiver"] = ["gmdev@hashedin.com"]
