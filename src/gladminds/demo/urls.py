783b350c7acf7f67140702856e3f4c8266038f85
priyanka66 <priyanka@hashedin.com>
2015-03-12 12:11:15 +0530

    GM-1115 Fix admin UI

--- src/gladminds/demo/urls.py
+++ src/gladminds/demo/urls.py
@@ -5,7 +5,7 @@ from gladminds.core.urls import api_v1
 
 urlpatterns = patterns('',
     url(r'^site-info/$', 'gladminds.demo.views.site_info', name='site_info'),
-    url(r'^admin/', include(brand_admin.urls)),
+    url(r'', include(brand_admin.urls)),
     url(r'', include(api_v1.urls)),
     url(r'', include(core_urls))
     
