fec65fb480979d5d0b8185e67e68222348287cfa
priyanka66 <priyanka@hashedin.com>
2015-03-25 12:23:31 +0530

    GM-1099 Run Daimler from core

--- src/gladminds/settings.py
+++ src/gladminds/settings.py
@@ -660,7 +660,11 @@ DISTRIBUTOR_SYNC_WSDL_URL = "http://local.bajaj.gladminds.co:8000/api/v1/distrib
 BRAND_META = {
                "bajaj": {"title": "Bajaj", "logo": "img/bajaj_logo.jpg", "tagline": "Bajaj Auto Pvt Ltd", "admin_url":"/admin/"},
                "demo": {"title": "Daimler", "logo": "daimler/img/daimler_logo.gif", "tagline": "2015 Daimler AG",
-                        "basecss": "/daimler/css/base.css","admin_url" :"/admin/"}
+                        "basecss": "/daimler/css/base.css","admin_url" :"/admin/"},
+              "daimler": {"title": "Daimler", "logo": "daimler/img/daimler_logo.gif", "tagline": "2015 Daimler AG",
+                        "basecss": "/daimler/css/base.css","admin_url" :"/admin/"},
+
+              
                }
 
 HOME_URLS = {
