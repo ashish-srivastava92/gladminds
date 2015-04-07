c807c735b6edcf4c66d9c62413dd6480dc428e14
Naureen Razi <naureen.razi@hahsedin.com>
2015-04-07 16:24:44 +0530

    GM-1216:: updated eco wsdl

--- src/gladminds/bajaj/services/coupons/import_feed.py
+++ src/gladminds/bajaj/services/coupons/import_feed.py
@@ -550,7 +550,11 @@ class ECOReleaseFeed(BaseFeed):
     def import_data(self):
         for eco_obj in self.data_source:
             try:
-                eco_release_obj = models.ECORelease(eco_number=eco_obj['eco_number'], eco_release_date=eco_obj['eco_release_date'],
+                if eco_obj['eco_release_date'] == "0000-00-00" or not eco_obj['eco_release_date']:
+                    eco_release_date=None
+                else:
+                    eco_release_date=datetime.strptime(eco_obj['eco_release_date'], "%Y-%m-%d")
+                eco_release_obj = models.ECORelease(eco_number=eco_obj['eco_number'], eco_release_date=eco_release_date,
                                                     eco_description=eco_obj['eco_description'], action=eco_obj['action'], parent_part=eco_obj['parent_part'],
                                                     add_part=eco_obj['add_part'], add_part_qty=eco_obj['add_part_qty'], add_part_rev=eco_obj['add_part_rev'],
                                                     add_part_loc_code=eco_obj['add_part_loc_code'], del_part=eco_obj['del_part'], del_part_qty=eco_obj['del_part_qty'],
@@ -609,7 +613,11 @@ class ECOImplementationFeed(BaseFeed):
     def import_data(self):
         for eco_obj in self.data_source:
             try:
-                eco_implementation_obj = models.ECOImplementation(change_no=eco_obj['change_no'],change_date=eco_obj['change_date'],
+                if eco_obj['change_date'] == "0000-00-00" or not eco_obj['change_date']:
+                    change_date=None
+                else:
+                    change_date=datetime.strptime(eco_obj['change_date'], "%Y-%m-%d")
+                eco_implementation_obj = models.ECOImplementation(change_no=eco_obj['change_no'],change_date=change_date,
                                                            change_time=eco_obj['change_time'],plant=eco_obj['plant'],
                                                            action=eco_obj['action'],parent_part=eco_obj['parent_part'],
                                                            added_part=eco_obj['added_part'],added_part_qty=eco_obj['added_part_qty'],
