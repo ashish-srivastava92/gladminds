70b403086427a5198f258ac21b1eb7ea91805e98
Naureen Razi <naureen.razi@hahsedin.com>
2015-04-10 16:05:56 +0530

    GM-000: added silent true

--- src/gladminds/core/managers/audit_manager.py
+++ src/gladminds/core/managers/audit_manager.py
@@ -13,7 +13,7 @@ def sms_log(brand, action='SENT', sender='+1 469-513-9856', receiver=None,
                                receiver=receiver, status=status,
                                message=message)
     sms_log.save(using=brand)
-    if settings.ENV not in ['qa','prod']:
+    if settings.ENV not in settings.IGNORE_ENV:
         LOGGER.post_event("sms_sent", {'receiver':receiver,
                                       'message':message,
                                       'brand':brand})
