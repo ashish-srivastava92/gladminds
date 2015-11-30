c430f4a6222df6b0d961cb253cb9343d961b4627
Ubuntu <ubuntu@ip-172-31-26-61.ec2.internal>
2015-11-30 04:39:36 +0000

    cganged DB host

--- src/gladminds/settings.py
+++ src/gladminds/settings.py
@@ -243,19 +243,19 @@ MANAGERS = ADMINS
 DATABASE_ROUTERS = ['gladminds.router.DatabaseAppsRouter']
 
 # for localhost
-DB_USER = os.environ.get('DB_USER', 'root')
-DB_HOST = os.environ.get('DB_HOST', '127.0.0.1')
-DB_PASSWORD = os.environ.get('DB_PASSWORD', 'gladminds')
+#DB_USER = os.environ.get('DB_USER', 'root')
+#DB_HOST = os.environ.get('DB_HOST', '127.0.0.1')
+#DB_PASSWORD = os.environ.get('DB_PASSWORD', 'gladminds')
 
 # # for server
-# DB_USER = os.environ.get('DB_USER', 'gladminds')
-# DB_HOST = os.environ.get('DB_HOST', 'gladminds-qa-2.chnnvvffqwop.us-east-1.rds.amazonaws.com')
-# DB_PASSWORD = os.environ.get('DB_PASSWORD', 'gladmindsqa2')
+DB_USER = os.environ.get('DB_USER', 'dssoffline')
+DB_HOST = os.environ.get('DB_HOST', 'dssoffline.chnnvvffqwop.us-east-1.rds.amazonaws.com') 
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'dssoffline')
 
 # for bajaj MC
-# DB_USER = os.environ.get('DB_USER', 'aftersell')
-# DB_HOST = os.environ.get('DB_HOST', 'aftersell-api.chnnvvffqwop.us-east-1.rds.amazonaws.com')
-# DB_PASSWORD = os.environ.get('DB_PASSWORD', 'aftersell321')
+#DB_USER = os.environ.get('DB_USER', 'aftersell')
+#DB_HOST = os.environ.get('DB_HOST', 'aftersell-api.chnnvvffqwop.us-east-1.rds.amazonaws.com')
+#DB_PASSWORD = os.environ.get('DB_PASSWORD', 'aftersell321')
 
 # for bajaj CV
 # DB_USER = os.environ.get('DB_USER', 'bajajcv')
