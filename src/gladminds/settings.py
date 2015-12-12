6df0b63ff1c31af7559ade15a0bc0929b90b133e
Ubuntu <ubuntu@ip-172-31-26-51.ec2.internal>
2015-12-12 10:38:49 +0000

    db

--- src/gladminds/settings.py
+++ src/gladminds/settings.py
@@ -250,15 +250,15 @@ DATABASE_ROUTERS = ['gladminds.router.DatabaseAppsRouter']
 # DB_HOST = os.environ.get('DB_HOST', '127.0.0.1')
 # DB_PASSWORD = os.environ.get('DB_PASSWORD', 'gladminds')
 
-# DB_USER = os.environ.get('DB_USER', 'aftersell')
-# DB_HOST = os.environ.get('DB_HOST', 'aftersell-api.chnnvvffqwop.us-east-1.rds.amazonaws.com')
-# DB_PASSWORD = os.environ.get('DB_PASSWORD', 'aftersell321')
+DB_USER = os.environ.get('DB_USER', 'aftersell')
+DB_HOST = os.environ.get('DB_HOST', 'aftersell-api.chnnvvffqwop.us-east-1.rds.amazonaws.com')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'aftersell321')
 
 # for server
 
-DB_USER = os.environ.get('DB_USER', 'root')
-DB_HOST = os.environ.get('DB_HOST', '127.0.0.1')
-DB_PASSWORD = os.environ.get('DB_PASSWORD', 'gladminds')
+#DB_USER = os.environ.get('DB_USER', 'root')
+#DB_HOST = os.environ.get('DB_HOST', '127.0.0.1')
+#DB_PASSWORD = os.environ.get('DB_PASSWORD', 'gladminds')
 
 # # for server
 
