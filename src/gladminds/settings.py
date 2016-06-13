af9afb906c548d633417f23c1258ba765b842cdc
Prashant Kumar <prashant.kumar@gladminds.co>
2016-06-13 23:16:57 +0530

    adding mail server in settings

--- src/gladminds/settings.py
+++ src/gladminds/settings.py
@@ -581,6 +581,7 @@ PUSH_NOTIFICATIONS_SETTINGS = {
 #     ),
 # }
 
+MAIL_SERVER = 'localhost'
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
